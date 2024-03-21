741a150ce36da30802f436fd2a7f084c83ac1f8a
Hui Zhou <hui.zhou@ansys.com>
2024-03-21 12:22:52 +0100

    Update examples/09-General/00_com_analysis.py
    Co-authored-by: Sébastien Morais <146729917+SMoraisAnsys@users.noreply.github.com>

--- examples/09-General/00_com_analysis.py
+++ examples/09-General/00_com_analysis.py
@@ -1,15 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     formats: ipynb,py
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.1
-# ---
-
 # # Channel Operating Margin (COM)
 # This example shows how you can use pyaedt for COM analysis.
 
