768353ba58e786aec8a1706740f977da9a333125
lvecchie <lorenzo.vecchietti@ansys.com>
2024-04-03 20:23:10 +0200

    refactoring graphic card example

--- examples/04-Icepak/Icepak_Example.py
+++ examples/04-Icepak/Icepak_Example.py
@@ -4,11 +4,12 @@
 # Icepak and postprocess results.
 # The example file is an Icepak project with a model that is already created and
 # has materials assigned.
+# Keywords: boundary conditions, 3D components
 
 # ## Perform required imports
 #
 # Perform required imports.
-
+# +
 import os
 
 from ansys.pyaedt.examples.constants import AEDT_VERSION
@@ -51,15 +52,16 @@ ipk.plot(
 #
 # Create source blocks on the CPU and memories.
 
-ipk.create_source_block("CPU", "25W")
-ipk.create_source_block(["MEMORY1", "MEMORY1_1"], "5W")
+ipk.create_source_block(object_name="CPU", input_power="25W")
+ipk.create_source_block(object_name=["MEMORY1", "MEMORY1_1"], input_power="5W")
 
 # ## Assign openings and grille
 #
 # Assign openings and a grille.
 
 region = ipk.modeler["Region"]
-ipk.assign_openings(air_faces=region.bottom_face_x.id)
+ipk.assign_pressure_free_opening(assignment=region.top_face_x.id, boundary_name="Outlet")
+ipk.assign_velocity_free_opening(assignment=region.bottom_face_x.id, boundary_name="Inlet", velocity=["1m_per_sec", "0m_per_sec", "0m_per_sec"],)
 ipk.assign_grille(air_faces=region.top_face_x.id, free_area_ratio=0.8)
 
 # ## Assign mesh regions
@@ -67,43 +69,61 @@ ipk.assign_grille(air_faces=region.top_face_x.id, free_area_ratio=0.8)
 # Assign a mesh region to the heat sink and CPU.
 
 mesh_region = ipk.mesh.assign_mesh_region(objectlist=["HEAT_SINK", "CPU"])
-mesh_region.UserSpecifiedSettings = True
-mesh_region.MaxElementSizeX = "3.35mm"
-mesh_region.MaxElementSizeY = "1.75mm"
-mesh_region.MaxElementSizeZ = "2.65mm"
-mesh_region.MaxLevels = "2"
+mesh_region.manual_settings = True
+mesh_region.settings["MaxElementSizeX"] = "3.35mm"
+mesh_region.settings["MaxElementSizeY"] = "1.75mm"
+mesh_region.settings["MaxElementSizeZ"] = "2.65mm"
+mesh_region.settings["MaxLevels"] = "2"
 mesh_region.update()
 
 # ## Assign point monitor
 #
-# Assign a point monitor and set it up.
+# Assign a point monitor.
 
-ipk.assign_point_monitor(
+m1 = ipk.assign_point_monitor(
     point_position=["-35mm", "3.6mm", "-86mm"], monitor_name="TemperatureMonitor1"
 )
-ipk.assign_point_monitor(point_position=["80mm", "14.243mm", "-55mm"], monitor_type="Speed")
+speed_monitors = []
+for x_pos in range(0, 80, 20):
+    m = ipk.assign_point_monitor(point_position=[f"{x_pos}mm", "14.243mm", "-55mm"], monitor_type="Speed")
+    speed_monitors.append(m)
+
+# ## Solve project
+# Create a setup, modify solver settings and run the simulation
 setup1 = ipk.create_setup()
 setup1.props["Flow Regime"] = "Turbulent"
 setup1.props["Convergence Criteria - Max Iterations"] = 5
 setup1.props["Linear Solver Type - Pressure"] = "flex"
 setup1.props["Linear Solver Type - Temperature"] = "flex"
 ipk.save_project()
+ipk.analyze()
 
-# ## Solve project and postprocess
-#
-# Solve the project and plot temperatures.
-
-# +
-quantity_name = "SurfTemperature"
+# ## PostProcess
+# Create a temperature plot
 surflist = [i.id for i in ipk.modeler["CPU"].faces]
 surflist += [i.id for i in ipk.modeler["MEMORY1"].faces]
 surflist += [i.id for i in ipk.modeler["MEMORY1_1"].faces]
 surflist += [i.id for i in ipk.modeler["ALPHA_MAIN_PCB"].faces]
+plot5 = ipk.post.create_fieldplot_surface(objlist=surflist, quantityName="SurfTemperature")
+plot5.export_image(full_path=os.path.join(temp_folder, "temperature.png"))
 
-plot5 = ipk.post.create_fieldplot_surface(surflist, "SurfTemperature")
+# Get the point monitor data.
+temperature_data = ipk.post.evaluate_monitor_quantity(monitor_name=m1, quantity_name="Temperature")
+temperature_data
 
-ipk.analyze()
-# -
+# It is also possible to get the data as pandas dataframe for advanced features.
+speed_fs = ipk.post.create_field_summary()
+for m_name in speed_monitors:
+    speed_fs.add_calculation(
+        entity="Monitor",
+        geometry="Volume",
+        geometry_name=m_name,
+        quantity="Speed"
+    )
+speed_data = speed_fs.get_field_summary_data(pandas_output=True)
+speed_data
+
+speed_data["Mean"].astype(float).plot(marker="o", ylabel="Speed", xlabel="Position", grid=True)
 
 # ## Release AEDT
 #
