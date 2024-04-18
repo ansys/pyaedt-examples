0f405e88ac4351288433c83b6dd57a18e82a54d8
Devin <38879940+dcrawforAtAnsys@users.noreply.github.com>
2024-04-18 15:53:22 +0200

    DOC: Add Circuit examples (#80)
    * DOC: Avoid running examples twice
    
    * CI: Add ON_CI environment variable
    
    * DOC: Only keep doctree to shorten CI/CD
    
    * CI: Use env var SPHINXBUILD_KEEP_DOCTREEDIR
    
    * ADD: All examples from pyaedt repo.
    
    * Update: 04-Static_ROM_Creation_And_Visualization.py
    
    * Update: Fix code style errors.
    
    * Update: Fix code style errors.
    
    * Update: Fix code style errors.
    
    * Update: Fix code style errors.
    
    * Update: Fix code style errors.
    
    * Update: Fix code style errors.
    
    * DELETE: intereference_gui.py\07-EMIT
    
    * FIX: typo in EDB_in_3DLayout.py
    
    * MAINT: Update pre-commit codespell version
    
    Note: Previous version was triggering a false positive in an
    example.
    
    * FIX: Use pyedb to instanciate EDB
    
    * MAINT: Update codespell configuration
    
    * FIX: New codespell catches
    
    * FIX: Put back exclude css files
    
    * FIX: Use EDB_VERSION and AEDT_VERSION
    
    * FIX: HFSS3DLayout example
    
    Note: When trying to access the loop resistance, an error happens and
    AEDT crashes which makes the example non ending.
    
    * ADD: 07-Circuit
    
    Refactor folder 07-Circuit to enable example editing and creation using Notebooks.
    
    * Update Circuit Examples
    
    * Fix typo
    
    * Fix Error
    
    - Fix a minor error in Circuit_Siwave_Multizones.py
    
    * TO BE REVERTED: remove other examples
    
    * TO BE REVERTED: remove failing example
    
    * Revert "TO BE REVERTED: remove other examples"
    
    This reverts commit 63238a0f7be7db8f9e6a86a2ec053f0f69d2d39c.
    
    * DOC: Add all index
    
    * Update examples/index.rst
    
    * Update examples/index.rst
    
    * Update examples/index.rst
    
    ---------
    
    Co-authored-by: Sebastien Morais <sebastien.morais@ansys.com>
    Co-authored-by: Samuel Lopez <85613111+Samuelopez-ansys@users.noreply.github.com>
    Co-authored-by: Sébastien Morais <146729917+SMoraisAnsys@users.noreply.github.com>

---
+++ examples/07-Circuit/Circuit_Siwave_Multizones.py
@@ -0,0 +1,115 @@
+# # Circuit: Simulate multi-zone layout with SIwave
+#
+# This example demonstates simulation of multiple zones with SIwave.
+
+# ## Perform required imports
+#
+# Perform required imports, which includes importing a section.
+
+# +
+import os.path
+import tempfile
+
+from ansys.pyaedt.examples.constants import EDB_VERSION
+import pyaedt
+from pyaedt import Circuit, Edb
+# -
+
+# ## Download file
+#
+# Download the EDB folder.
+
+temp_dir = tempfile.TemporaryDirectory(suffix=".ansys")
+edb_file = pyaedt.downloads.download_file(directory="edb/siwave_multi_zones.aedb",
+                                          destination=temp_dir.name)
+work_folder = os.path.join(temp_dir.name, "work")
+aedt_file = os.path.splitext(edb_file)[0] + ".aedt"
+circuit_project_file = os.path.join(temp_dir.name, "multizone_clipped_circuit.aedt")
+print(edb_file)
+
+
+# ## AEDT version
+#
+# Sets the AEDT version.
+
+edb_version = EDB_VERSION
+
+# ## Ground net
+#
+# Common reference net used across all sub-designs, Mandatory for this work flow.
+
+common_reference_net = "GND"
+
+# ## Load the Project
+#
+# Load initial Edb file, checking if aedt file exists and remove to allow Edb loading.
+
+if os.path.isfile(aedt_file):
+    os.remove(aedt_file)
+edb = Edb(edbversion=edb_version, edbpath=edb_file)
+
+# ## Project zones
+#
+# Copy project zone into sub project.
+
+edb_zones = edb.copy_zones(working_directory=work_folder)
+
+# ## Split zones
+#
+# Clip sub-designs along with corresponding zone definition
+# and create port of clipped signal traces.
+
+defined_ports, project_connexions = edb.cutout_multizone_layout(edb_zones, common_reference_net)
+
+# ## Circuit
+#
+# Create circuit design, import all sub-project as EM model and connect
+# all corresponding pins in circuit.
+
+circuit = Circuit(specified_version=edb_version, projectname=circuit_project_file)
+circuit.connect_circuit_models_from_multi_zone_cutout(
+    project_connections=project_connexions,
+    edb_zones_dict=edb_zones,
+    ports=defined_ports,
+    model_inc=70,
+)
+
+# ## Setup
+#
+# Add Nexxim LNA simulation setup.
+
+circuit_setup = circuit.create_setup("Pyedt_LNA")
+
+# ## Frequency sweep
+#
+# Add frequency sweep from 0GHt to 20GHz with 10NHz frequency step.
+
+circuit_setup.props["SweepDefinition"]["Data"] = "LIN {} {} {}".format("0GHz", "20GHz", "10MHz")
+
+# ## Start simulation
+#
+# Analyze all siwave projects and solves the circuit.
+
+circuit.analyze()
+
+# Define differential pairs
+
+circuit.set_differential_pair(
+    diff_name="U0", positive_terminal="U0.via_38.B2B_SIGP", negative_terminal="U0.via_39.B2B_SIGN"
+)
+circuit.set_differential_pair(
+    diff_name="U1", positive_terminal="U1.via_32.B2B_SIGP", negative_terminal="U1.via_33.B2B_SIGN"
+)
+
+# Plot results
+
+circuit.post.create_report(
+    expressions=["dB(S(U0,U0))", "dB(S(U1,U0))"], context="Differential Pairs"
+)
+
+# ## Release AEDT desktop
+#
+
+circuit.release_desktop()
+
+temp_dir.cleanup()  # Remove the temporary working folder and all project files
