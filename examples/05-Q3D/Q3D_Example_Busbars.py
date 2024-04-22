d980c8978c47bbdea21690f2da44049f736044f7
Giulia Malinverno <giulia.malinverno@ansys.com>
2024-04-22 14:34:04 +0200

    improve q3d busbar

--- examples/05-Q3D/Q3D_Example_Busbars.py
+++ examples/05-Q3D/Q3D_Example_Busbars.py
@@ -8,16 +8,16 @@
 # Perform required imports.
 
 import os
+import tempfile
 
-from ansys.pyaedt.examples.constants import AEDT_VERSION
+from ansys.pyaedt.examples.constants import AEDT_VERSION, NUM_CORES
 import pyaedt
 
-# ## Set non-graphical mode
+# ## Create temporary directory
 #
-# Set non-graphical mode.
-# You can set ``non_graphical`` either to ``True`` or ``False``.
+# Create temporary directory.
 
-non_graphical = False
+temp_dir = tempfile.TemporaryDirectory(suffix=".ansys")
 
 # ## Set debugger mode
 #
@@ -32,10 +32,10 @@ pyaedt.settings.enable_debug_internal_methods_logger = False
 #
 # Launch AEDT 2023 R2 in graphical mode and launch Q3D Extractor. This example uses SI units.
 
-q = pyaedt.Q3d(
+q3d = pyaedt.Q3d(
     projectname=pyaedt.generate_unique_project_name(),
     specified_version=AEDT_VERSION,
-    non_graphical=non_graphical,
+    non_graphical=False,
     new_desktop_session=True,
 )
 
@@ -44,41 +44,43 @@ q = pyaedt.Q3d(
 # Create polylines for three busbars and a box for the substrate.
 
 # +
-b1 = q.modeler.create_polyline(
-    [[0, 0, 0], [-100, 0, 0]],
+b1 = q3d.modeler.create_polyline(
+    position_list=[[0, 0, 0], [-100, 0, 0]],
     name="Bar1",
     matname="copper",
     xsection_type="Rectangle",
     xsection_width="5mm",
     xsection_height="1mm",
 )
-q.modeler["Bar1"].color = (255, 0, 0)
+q3d.modeler["Bar1"].color = (255, 0, 0)
 
-q.modeler.create_polyline(
-    [[0, -15, 0], [-150, -15, 0]],
+q3d.modeler.create_polyline(
+    points=[[0, -15, 0], [-150, -15, 0]],
     name="Bar2",
     matname="aluminum",
     xsection_type="Rectangle",
     xsection_width="5mm",
     xsection_height="1mm",
 )
-q.modeler["Bar2"].color = (0, 255, 0)
+q3d.modeler["Bar2"].color = (0, 255, 0)
 
-q.modeler.create_polyline(
-    [[0, -30, 0], [-175, -30, 0], [-175, -10, 0]],
+q3d.modeler.create_polyline(
+    points=[[0, -30, 0], [-175, -30, 0], [-175, -10, 0]],
     name="Bar3",
     matname="copper",
     xsection_type="Rectangle",
     xsection_width="5mm",
     xsection_height="1mm",
 )
-q.modeler["Bar3"].color = (0, 0, 255)
+q3d.modeler["Bar3"].color = (0, 0, 255)
 
-q.modeler.create_box([50, 30, -0.5], [-250, -100, -3], name="substrate", matname="FR4_epoxy")
-q.modeler["substrate"].color = (128, 128, 128)
-q.modeler["substrate"].transparency = 0.8
+q3d.modeler.create_box(
+    origin=[50, 30, -0.5], sizes=[-250, -100, -3], name="substrate", matname="FR4_epoxy"
+)
+q3d.modeler["substrate"].color = (128, 128, 128)
+q3d.modeler["substrate"].transparency = 0.8
 
-q.plot(show=False, export_path=os.path.join(q.working_directory, "Q3D.jpg"), plot_air_objects=False)
+q3d.plot(show=False, export_path=os.path.join(temp_dir.name, "Q3D.jpg"), plot_air_objects=False)
 # -
 
 # ## Set up boundaries
@@ -87,73 +89,71 @@ q.plot(show=False, export_path=os.path.join(q.working_directory, "Q3D.jpg"), plo
 # There is a source and sink for each busbar.
 
 # +
-q.auto_identify_nets()
+q3d.auto_identify_nets()
 
-q.source("Bar1", axisdir=q.AxisDir.XPos, name="Source1")
-q.sink("Bar1", axisdir=q.AxisDir.XNeg, name="Sink1")
+q3d.source(assignment="Bar1", axisdir=q3d.AxisDir.XPos, name="Source1")
+q3d.sink(assignment="Bar1", axisdir=q3d.AxisDir.XNeg, name="Sink1")
 
-q.source("Bar2", axisdir=q.AxisDir.XPos, name="Source2")
-q.sink("Bar2", axisdir=q.AxisDir.XNeg, name="Sink2")
-q.source("Bar3", axisdir=q.AxisDir.XPos, name="Source3")
-bar3_sink = q.sink("Bar3", axisdir=q.AxisDir.YPos)
-bar3_sink.name = "Sink3"
+q3d.source(assignment="Bar2", axisdir=q3d.AxisDir.XPos, name="Source2")
+q3d.sink(assignment="Bar2", axisdir=q3d.AxisDir.XNeg, name="Sink2")
+q3d.source(assignment="Bar3", axisdir=q3d.AxisDir.XPos, name="Source3")
+bar3_sink = q3d.sink(assignment="Bar3", axisdir=q3d.AxisDir.YPos, name="Sink3")
 # -
 
 # ## Print information
 #
 # Use the different methods available to print net and terminal information.
 
-print(q.nets)
-print(q.net_sinks("Bar1"))
-print(q.net_sinks("Bar2"))
-print(q.net_sinks("Bar3"))
-print(q.net_sources("Bar1"))
-print(q.net_sources("Bar2"))
-print(q.net_sources("Bar3"))
+print(q3d.nets)
+print(q3d.net_sinks("Bar1"))
+print(q3d.net_sinks("Bar2"))
+print(q3d.net_sinks("Bar3"))
+print(q3d.net_sources("Bar1"))
+print(q3d.net_sources("Bar2"))
+print(q3d.net_sources("Bar3"))
 
 # ## Create setup
 #
 # Create a setup for Q3D Extractor and add a sweep that defines the adaptive
 # frequency value.
 
-setup1 = q.create_setup(props={"AdaptiveFreq": "100MHz"})
-sw1 = setup1.add_sweep()
-sw1.props["RangeStart"] = "1MHz"
-sw1.props["RangeEnd"] = "100MHz"
-sw1.props["RangeStep"] = "5MHz"
-sw1.update()
+setup1 = q3d.create_setup(props={"AdaptiveFreq": "100MHz"})
+sweep = setup1.add_sweep()
+sweep.props["RangeStart"] = "1MHz"
+sweep.props["RangeEnd"] = "100MHz"
+sweep.props["RangeStep"] = "5MHz"
+sweep.update()
 
 # ## Get curves to plot
 #
 # Get the curves to plot. The following code simplifies the way to get curves.
 
-data_plot_self = q.matrices[0].get_sources_for_plot(get_self_terms=True, get_mutual_terms=False)
-data_plot_mutual = q.get_traces_for_plot(get_self_terms=False, get_mutual_terms=True, category="C")
-data_plot_self
-data_plot_mutual
+data_plot_self = q3d.matrices[0].get_sources_for_plot(get_self_terms=True, get_mutual_terms=False)
+data_plot_mutual = q3d.get_traces_for_plot(
+    get_self_terms=False, get_mutual_terms=True, category="C"
+)
 
 # ## Create rectangular plot
 #
 # Create a rectangular plot and a data table.
 
-q.post.create_report(expressions=data_plot_self)
-q.post.create_report(expressions=data_plot_mutual, context="Original", plot_type="Data Table")
+q3d.post.create_report(expressions=data_plot_self)
+q3d.post.create_report(expressions=data_plot_mutual, context="Original", plot_type="Data Table")
 
 # ## Solve setup
 #
 # Solve the setup.
 
-q.analyze()
-q.save_project()
+q3d.analyze(num_cores=NUM_CORES)
+q3d.save_project()
 
 # ## Get report data
 #
 # Get the report data into a data structure that allows you to manipulate it.
 
-a = q.post.get_solution_data(expressions=data_plot_self, context="Original")
-a.intrinsics["Freq"]
-a.data_magnitude()
-a.plot()
+data = q3d.post.get_solution_data(expressions=data_plot_self, context="Original")
+data.data_magnitude()
+data.plot()
 
 # ## Close AEDT
 #
@@ -162,4 +162,5 @@ a.plot()
 
 pyaedt.settings.enable_debug_logger = False
 pyaedt.settings.enable_debug_methods_argument_logger = False
-q.release_desktop(close_projects=True, close_desktop=True)
+q3d.release_desktop(close_projects=True, close_desktop=True)
+temp_dir.cleanup()
