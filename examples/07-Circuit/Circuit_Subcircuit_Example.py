873a7ff8b540b70af217b68f1b00a8276f35e1a6
Sebastien Morais <sebastien.morais@ansys.com>
2024-04-29 09:12:50 +0200

    Revert "TO BE REVERTED: Remove other examples"
    This reverts commit 1408b59c1a4c9b8d7eccd67c8ba732f127193fef.

---
+++ examples/07-Circuit/Circuit_Subcircuit_Example.py
@@ -0,0 +1,74 @@
+# # Circuit: schematic subcircuit management
+#
+# This example shows how to add a subcircuit to a circuit design.
+# In this example, the focus is changed within the hierarchy between
+# the child subcircuit and the parent design.
+
+# ## Imports
+#
+# Perform the required import.
+
+import os
+import tempfile
+
+from ansys.pyaedt.examples.constants import AEDT_VERSION
+import pyaedt
+
+# ## Set non-graphical mode
+#
+# Set non-graphical mode.
+# You can set ``non_graphical`` either to ``True`` or ``False``.
+
+# ## Launch AEDT with Circuit
+#
+# Launch AEDT in graphical mode. Instantite an instance of the ``Circuit`` class.
+
+non_graphical = False
+temp_dir = tempfile.TemporaryDirectory(suffix=".ansys")
+circuit = pyaedt.Circuit(
+    projectname=os.path.join(temp_dir.name, "SubcircuitExample"),
+    designname="SimpleExample",
+    specified_version=AEDT_VERSION,
+    non_graphical=non_graphical,
+    new_desktop_session=True,
+)
+circuit.modeler.schematic_units = "mil"
+
+# ## Add Subcircuit
+#
+# Add a new subcircuit to the previously created circuit design, creating a
+# child circuit. Push this child circuit down into the child subcircuit.
+
+subcircuit = circuit.modeler.schematic.create_subcircuit(location=[0.0, 0.0])
+subcircuit_name = subcircuit.composed_name
+circuit.push_down(subcircuit)
+
+# ## Parametrization
+#
+# Parametrize the subcircuit and add a resistor, inductor, and a capacitor with
+# the parameter values. Components are connected in series
+# and the focus is changed to the parent schematic using
+# the ``pop_up`` method.
+
+circuit.variable_manager.set_variable(variable_name="R_val", expression="35ohm")
+circuit.variable_manager.set_variable(variable_name="L_val", expression="1e-7H")
+circuit.variable_manager.set_variable(variable_name="C_val", expression="5e-10F")
+p1 = circuit.modeler.schematic.create_interface_port(name="In")
+r1 = circuit.modeler.schematic.create_resistor(value="R_val")
+l1 = circuit.modeler.schematic.create_inductor(value="L_val")
+c1 = circuit.modeler.schematic.create_capacitor(value="C_val")
+p2 = circuit.modeler.schematic.create_interface_port(name="Out")
+circuit.modeler.schematic.connect_components_in_series(
+    components_to_connect=[p1, r1, l1, c1, p2], use_wire=True
+)
+circuit.pop_up()
+
+
+# ## Done
+#
+# Release AEDT and clean up the temporary files. If you run this example locally and
+# want to keep the files, the project folder name can be retrieved from ``temp_file.name``.
+
+circuit.release_desktop(True, True)
+
+temp_dir.cleanup()  # Clean up temporary folder and remove project data.
--- examples/index.rst
+++ examples/index.rst
@@ -5,7 +5,15 @@
    :maxdepth: 2
 
 
+   twin_builder/index
+   01-Modeling-Setup/index
+   02-HFSS/index
    02-SBR+/index
+   03-Maxwell/index
+   06-Multiphysics/index
+   07-Circuit/index
+   07-EMIT/index
+   EDB/index
 
 Examples
 ========
