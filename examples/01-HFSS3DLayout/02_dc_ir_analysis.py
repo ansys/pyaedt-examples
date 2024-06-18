fe6d86d457eeaf526f493df5f768eb725268b612
Sebastien Morais <sebastien.morais@ansys.com>
2024-06-18 16:16:48 +0200

    Revert "TBR: Focus on Circuit issue"
    This reverts commit a9889b20bbe23eeb1d9df5e8848d07dbb5e0f5b2.

---
+++ examples/01-HFSS3DLayout/02_dc_ir_analysis.py
@@ -0,0 +1,235 @@
+# # HFSS 3D Layout: DC IR Analysis
+# This example shows how to configure EDB for DC IR analysis, and load EDB into the 3D Layout UI for analysis and
+# post-processing.
+# - Set up EDB
+#     - Edit via padstack
+#     - Assign SPICE model to components
+#     - Create pin groups
+#     - Create voltage and current sources
+#     - Create SIwave DC anaylsis
+#     - Create cutout
+# - Import EDB into HFSS 3D Layout
+#     - Analyze
+#     - Get DC IR analysis results
+
+
+# # Preparation
+# Import required packages
+
+# +
+import os
+import json
+import tempfile
+import time
+from pyedb import Edb
+from pyaedt import Hfss3dLayout
+from pyaedt.downloads import download_file
+try:
+    from ansys.pyaedt.examples.constants import AEDT_VERSION
+except:
+    AEDT_VERSION = "2024.1"
+
+NG_MODE = False
+
+temp_folder = tempfile.TemporaryDirectory(suffix=".ansys")
+# -
+
+# Download example board.
+
+aedb = download_file(
+    source="edb/ANSYS-HSD_V1.aedb", destination=temp_folder.name
+)
+download_file(
+    source="spice", name="ferrite_bead_BLM15BX750SZ1.mod", destination=temp_folder.name
+)
+
+# # Create a configuration file
+# In this example, we are going to use a configure file to set up layout for analysis.
+# ## Initialize a dictionary
+
+cfg = dict()
+
+# Define model library paths.
+
+cfg["general"] = {
+    "s_parameter_library": os.path.join(temp_folder.name, "touchstone"),
+    "spice_model_library": os.path.join(temp_folder.name, "spice")
+}
+
+# ## Change via hole size and plating thickness
+
+cfg["padstacks"] = {
+    "definitions": [
+        {
+            "name": "v40h15-3",
+            "hole_diameter": "0.2mm",
+            "hole_plating_thickness": "25um"
+        }
+    ],
+}
+
+# ## Assign SPICE models
+
+cfg["spice_models"] = [
+    {
+        "name": "ferrite_bead_BLM15BX750SZ1",  # Give a name to the SPICE Mode.
+        "component_definition": "COIL-1008CS_V",  # Part name of the components
+        "file_path": "ferrite_bead_BLM15BX750SZ1.mod",  # File name or full file path to the SPICE file.
+        "sub_circuit_name": "BLM15BX750SZ1",
+        "apply_to_all": True,  # If True, SPICE model is to be assigned to all components share the same part name.
+        # If False, only assign SPICE model to components in "components".
+        "components": []
+    }
+]
+
+# ## Create a Voltage Source
+# Create a voltage source from net.
+
+cfg["sources"] = [
+    {
+        "name": "VSOURCE_5V",
+        "reference_designator": "U4",
+        "type": "voltage",
+        "magnitude": 5,
+        "positive_terminal": {
+            "net": "5V"
+        },
+        "negative_terminal": {
+            "net": "GND"
+        }
+    }
+]
+
+# ## Create Current Sources
+# Create current sources between net and pin group.
+
+cfg["pin_groups"] = [
+    {
+        "name": "J5_GND",
+        "reference_designator": "J5",
+        "net": "GND"
+    }
+]
+
+cfg["sources"].append(
+    {
+        "name": "J5_VCCR",
+        "reference_designator": "J5",
+        "type": "current",
+        "magnitude": 0.5,
+        "positive_terminal": {
+            "net": "SFPA_VCCR"
+        },
+        "negative_terminal": {
+            "pin_group": "J5_GND"  # Defined in "pin_groups" section.
+        }
+    }
+)
+cfg["sources"].append(
+    {
+        "name": "J5_VCCT",
+        "reference_designator": "J5",
+        "type": "current",
+        "magnitude": 0.5,
+        "positive_terminal": {
+            "net": "SFPA_VCCT"
+        },
+        "negative_terminal": {
+            "pin_group": "J5_GND"  # Defined in "pin_groups" section.
+        }
+    }
+)
+
+# ## Create SIwave DC analysis
+
+cfg["setups"] = [
+    {
+        "name": "siwave_dc",
+        "type": "siwave_dc",
+        "dc_slider_position": 0
+    }
+]
+
+# ## Do cutout
+
+cfg["operations"] = {
+    "cutout": {
+        "signal_list": ["SFPA_VCCR", "SFPA_VCCT", "5V"],
+        "reference_list": ["GND"],
+        "extent_type": "ConvexHull",
+        "expansion_size": 0.002,
+        "use_round_corner": False,
+        "output_aedb_path": "",
+        "open_cutout_at_end": True,
+        "use_pyaedt_cutout": True,
+        "number_of_threads": 4,
+        "use_pyaedt_extent_computing": True,
+        "extent_defeature": 0,
+        "remove_single_pin_components": False,
+        "custom_extent": "",
+        "custom_extent_units": "mm",
+        "include_partial_instances": False,
+        "keep_voids": True,
+        "check_terminals": False,
+        "include_pingroups": False,
+        "expansion_factor": 0,
+        "maximum_iterations": 10,
+        "preserve_components_with_model": False,
+        "simple_pad_check": True,
+        "keep_lines_as_path": False
+    }
+}
+
+# ## Save configuration as a JSON file
+
+pi_json = os.path.join(temp_folder.name, "pi.json")
+with open(pi_json, "w") as f:
+    json.dump(cfg, f, indent=4, ensure_ascii=False)
+
+# # Load configuration into EDB
+
+# Load configuration from JSON
+
+edbapp = Edb(aedb, edbversion=AEDT_VERSION)
+edbapp.configuration.load(config_file=pi_json)
+edbapp.configuration.run()
+edbapp.save()
+edbapp.close()
+
+# The configured EDB file is saved in a temp folder.
+
+print(temp_folder.name)
+
+# # Analyze DCIR with SIwave
+#
+# The 3D Layout interface to SIwave is used to open the EDB and run the DCIR analysis
+# using SIwave
+
+# ## Load edb into 3D Layout.
+
+siw = Hfss3dLayout(
+    aedb,
+    specified_version=AEDT_VERSION,
+    non_graphical=NG_MODE,
+    new_desktop_session=True
+)
+
+# ## Analyze
+
+siw.analyze()
+
+# ## Get DC IR results
+
+siw.get_dcir_element_data_current_source("siwave_dc")
+
+# ## Shut Down Electronics Desktop
+
+siw.close_desktop()
+
+# ## Cleanup
+#
+# All project files are saved in the folder ``temp_file.dir``. If you've run this example as a Jupyter notbook you 
+# can retrieve those project files. The following cell removes all temporary files, including the project folder.
+
+time.sleep(3)
+temp_folder.cleanup()
---
+++ examples/03-Maxwell/Maxwell2D_Electrostatic.py
@@ -0,0 +1,209 @@
+# # Maxwell 2D Electrostatic analysis
+
+# This example shows how you can use PyAEDT to create a Maxwell 2D electrostatic analysis.
+# It shows how to create the geometry, load material properties from an Excel file and
+# set up the mesh settings. Moreover, it focuses on post-processing operations, in particular how to
+# plot field line traces, relevant for an electrostatic analysis.
+
+# ## Perform required imports
+#
+# Perform required imports.
+
+from ansys.pyaedt.examples.constants import AEDT_VERSION
+import pyaedt
+
+# ## Initialize Maxwell 2D
+#
+# Initialize Maxwell 2D, providing the version, path to the project, and the design
+# name and type.
+
+aedt_version = AEDT_VERSION
+setup_name = "MySetupAuto"
+solver = "Electrostatic"
+design_name = "Design1"
+project_name = pyaedt.generate_unique_project_name()
+non_graphical = False
+
+# ## Download .xlsx file
+#
+# Set local temporary folder to export the .xlsx file to.
+
+file_name_xlsx = pyaedt.downloads.download_file(source="field_line_traces", name="my_copper.xlsx")
+
+# ## Initialize dictionaries
+#
+# Initialize dictionaries that contain all the definitions for the design variables.
+
+# +
+geom_params_circle = {
+    "circle_x0": "-10mm",
+    "circle_y0": "0mm",
+    "circle_z0": "0mm",
+    "circle_axis": "Z",
+    "circle_radius": "1mm",
+}
+
+geom_params_rectangle = {
+    "r_x0": "1mm",
+    "r_y0": "5mm",
+    "r_z0": "0mm",
+    "r_axis": "Z",
+    "r_dx": "-1mm",
+    "r_dy": "-10mm",
+}
+# -
+
+# ## Launch Maxwell 2D
+#
+# Launch Maxwell 2D and save the project.
+
+m2d = pyaedt.Maxwell2d(
+    projectname=project_name,
+    specified_version=aedt_version,
+    designname=design_name,
+    solution_type=solver,
+    new_desktop_session=True,
+    non_graphical=non_graphical,
+)
+
+# ## Create object to access 2D modeler
+#
+# Create the object ``mod_2d`` to access the 2D modeler easily.
+
+mod_2d = m2d.modeler
+mod_2d.delete()
+mod_2d.model_units = "mm"
+
+# ## Define variables from dictionaries
+#
+# Define design variables from the created dictionaries.
+
+for k, v in geom_params_circle.items():
+    m2d[k] = v
+for k, v in geom_params_rectangle.items():
+    m2d[k] = v
+
+# ## Read materials from .xslx file
+#
+# Read materials from .xslx file into and set into design.
+
+mats = m2d.materials.import_materials_from_excel(file_name_xlsx)
+
+# ## Create design geometries
+#
+# Create rectangle and a circle and assign the material read from the .xlsx file.
+# Create two new polylines and a region.
+
+# +
+rect = mod_2d.create_rectangle(
+    origin=["r_x0", "r_y0", "r_z0"],
+    sizes=["r_dx", "r_dy", 0],
+    name="Ground",
+    material=mats[0],
+)
+rect.color = (0, 0, 255)  # rgb
+rect.solve_inside = False
+
+circle = mod_2d.create_circle(
+    origin=["circle_x0", "circle_y0", "circle_z0"],
+    radius="circle_radius",
+    num_sides="0",
+    is_covered=True,
+    name="Electrode",
+    material=mats[0],
+)
+circle.color = (0, 0, 255)  # rgb
+circle.solve_inside = False
+
+poly1_points = [[-9, 2, 0], [-4, 2, 0], [2, -2, 0], [8, 2, 0]]
+poly2_points = [[-9, 0, 0], [9, 0, 0]]
+poly1_id = mod_2d.create_polyline(points=poly1_points, segment_type="Spline", name="Poly1")
+poly2_id = mod_2d.create_polyline(points=poly2_points, name="Poly2")
+mod_2d.split(assignment=[poly1_id, poly2_id], plane="YZ", sides="NegativeOnly")
+mod_2d.create_region(pad_value=[20, 100, 20, 100])
+# -
+
+# ## Define excitations
+#
+# Assign voltage excitations to rectangle and circle.
+
+m2d.assign_voltage(rect.id, amplitude=0, name="Ground")
+m2d.assign_voltage(circle.id, amplitude=50e6, name="50kV")
+
+# ## Create initial mesh settings
+#
+# Assign a surface mesh to the rectangle.
+
+m2d.mesh.assign_surface_mesh_manual(assignment=["Ground"], surf_dev=0.001)
+
+# ## Create, validate and analyze the setup
+#
+# Create, update, validate and analyze the setup.
+
+setup = m2d.create_setup(name=setup_name)
+setup.props["PercentError"] = 0.5
+setup.update()
+m2d.validate_simple()
+m2d.analyze_setup(setup_name)
+
+# ## Evaluate the E Field tangential component
+#
+# Evaluate the E Field tangential component along the given polylines.
+# Add these operations to the Named Expression list in Field Calculator.
+
+fields = m2d.ofieldsreporter
+fields.CalcStack("clear")
+fields.EnterQty("E")
+fields.EnterEdge("Poly1")
+fields.CalcOp("Tangent")
+fields.CalcOp("Dot")
+fields.AddNamedExpression("e_tan_poly1", "Fields")
+fields.EnterQty("E")
+fields.EnterEdge("Poly2")
+fields.CalcOp("Tangent")
+fields.CalcOp("Dot")
+fields.AddNamedExpression("e_tan_poly2", "Fields")
+
+# ## Create Field Line Traces Plot
+#
+# Create Field Line Traces Plot specifying as seeding faces
+# the ground, the electrode and the region
+# and as ``In surface objects`` only the region.
+
+plot = m2d.post.create_fieldplot_line_traces(
+    seeding_faces=["Ground", "Electrode", "Region"],
+    in_volume_tracing_objs=["Region"],
+    plot_name="LineTracesTest",
+)
+
+# ## Update Field Line Traces Plot
+#
+# Update field line traces plot.
+# Update seeding points number, line style and line width.
+
+plot.SeedingPointsNumber = 20
+plot.LineStyle = "Cylinder"
+plot.LineWidth = 3
+plot.update()
+
+# ## Export field line traces plot
+#
+# Export field line traces plot.
+# For field lint traces plot, the export file format is ``.fldplt``.
+
+m2d.post.export_field_plot(
+    plot_name="LineTracesTest", output_dir=m2d.toolkit_directory, file_format="fldplt"
+)
+
+# ## Export the mesh field plot
+#
+# Export the mesh in ``aedtplt`` format.
+
+m2d.post.export_mesh_obj(setup_name=m2d.nominal_adaptive)
+
+# ## Save project and close AEDT
+#
+# Save the project and close AEDT.
+
+m2d.save_project()
+m2d.release_desktop()
