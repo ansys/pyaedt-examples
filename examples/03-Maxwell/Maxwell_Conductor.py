14f5818ff9a71ecf28f5611082ceb2aa03987f43
Giulia Malinverno <giulia.malinverno@ansys.com>
2024-07-22 14:59:05 +0200

    magnet -> conductor

--- examples/03-Maxwell/Maxwell_Conductor.py
+++ examples/03-Maxwell/Maxwell_Conductor.py
@@ -1,4 +1,4 @@
-# # Maxwell 3D: magnet DC analysis
+# # Maxwell 3D: Electro DC analysis
 
 # This example shows how you can use PyAEDT to create a Maxwell DC analysis,
 # compute mass center, and move coordinate systems.
@@ -10,6 +10,7 @@
 import os
 import tempfile
 import time
+
 from pyaedt import Maxwell3d
 
 # ## Define constants
@@ -33,7 +34,7 @@ temp_folder = tempfile.TemporaryDirectory(suffix=".ansys")
 # the project name, the version and the graphical mode.
 
 m3d = Maxwell3d(
-    project=os.path.join(temp_folder.name, "magnet_example"),
+    project=os.path.join(temp_folder.name, "conductor_example"),
     version=AEDT_VERSION,
     new_desktop=True,
     non_graphical=NG_MODE,
@@ -45,19 +46,19 @@ m3d = Maxwell3d(
 
 m3d.solution_type = m3d.SOLUTIONS.Maxwell3d.ElectroDCConduction
 
-# ## Create magnet
+# ## Create conductor
 #
-# Create a permanent magnet using material suing "Alinco9" whic is pre-defined in the Maxwell material library.
+# Create a conductor using "copper" which is pre-defined in the Maxwell material library.
 
-magnet = m3d.modeler.create_box(
-    origin=[7, 4, 22], sizes=[10, 5, 30], name="Magnet", material="Alnico9"
+conductor = m3d.modeler.create_box(
+    origin=[7, 4, 22], sizes=[10, 5, 30], name="Conductor", material="copper"
 )
 
 # ## Create setup and assign voltage
 #
 # Create the setup and assign a voltage.
 
-m3d.assign_voltage(assignment=magnet.faces, amplitude=0)
+m3d.assign_voltage(assignment=conductor.faces, amplitude=0)
 m3d.create_setup()
 
 # ## Plot model
@@ -81,15 +82,15 @@ m3d.analyze()
 # Compute mass center using the fields calculator.
 
 m3d.post.ofieldsreporter.EnterScalarFunc("X")
-m3d.post.ofieldsreporter.EnterVol(magnet.name)
+m3d.post.ofieldsreporter.EnterVol(conductor.name)
 m3d.post.ofieldsreporter.CalcOp("Mean")
 m3d.post.ofieldsreporter.AddNamedExpression("CM_X", "Fields")
 m3d.post.ofieldsreporter.EnterScalarFunc("Y")
-m3d.post.ofieldsreporter.EnterVol(magnet.name)
+m3d.post.ofieldsreporter.EnterVol(conductor.name)
 m3d.post.ofieldsreporter.CalcOp("Mean")
 m3d.post.ofieldsreporter.AddNamedExpression("CM_Y", "Fields")
 m3d.post.ofieldsreporter.EnterScalarFunc("Z")
-m3d.post.ofieldsreporter.EnterVol(magnet.name)
+m3d.post.ofieldsreporter.EnterVol(conductor.name)
 m3d.post.ofieldsreporter.CalcOp("Mean")
 m3d.post.ofieldsreporter.AddNamedExpression("CM_Z", "Fields")
 m3d.post.ofieldsreporter.CalcStack("clear")
@@ -106,24 +107,28 @@ zval = m3d.post.get_scalar_field_value(quantity="CM_Z")
 #
 # Create variables with mass center values.
 
-m3d[magnet.name + "x"] = str(xval * 1e3) + "mm"
-m3d[magnet.name + "y"] = str(yval * 1e3) + "mm"
-m3d[magnet.name + "z"] = str(zval * 1e3) + "mm"
+m3d[conductor.name + "x"] = str(xval * 1e3) + "mm"
+m3d[conductor.name + "y"] = str(yval * 1e3) + "mm"
+m3d[conductor.name + "z"] = str(zval * 1e3) + "mm"
 
 # ## Create coordinate system
 #
 # Create a parametric coordinate system.
 
 cs1 = m3d.modeler.create_coordinate_system(
-    origin=[magnet.name + "x", magnet.name + "y", magnet.name + "z"],
+    origin=[conductor.name + "x", conductor.name + "y", conductor.name + "z"],
     reference_cs="Global",
-    name=magnet.name + "CS",
+    name=conductor.name + "CS",
 )
 
 # ## Release AEDT and clean up temporary directory
 #
-# Release AEDT and remove both the project and temporary directory. If you'd like to retrieve the project data for subsequent use, the temporary folder name is given by ``temp_folder.name``.
+# Release AEDT and remove both the project and temporary directory.
+# If you'd like to retrieve the project data for subsequent use,
+# the temporary folder name is given by ``temp_folder.name``.
 
 m3d.release_desktop(close_projects=True, close_desktop=True)
-time.sleep(3)  # Allow time for Electronics Desktop to close before cleaning the temporary folder.
+time.sleep(
+    3
+)  # Allow time for Electronics Desktop to close before cleaning the temporary folder.
 temp_folder.cleanup()
