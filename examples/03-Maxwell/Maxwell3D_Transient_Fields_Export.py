bda55a36dab891d6c2fac6c35e5d8ca8606ecd88
Giulia Malinverno <giulia.malinverno@ansys.com>
2024-08-08 16:16:07 +0200

    add post-processing

--- examples/03-Maxwell/Maxwell3D_Transient_Fields_Export.py
+++ examples/03-Maxwell/Maxwell3D_Transient_Fields_Export.py
@@ -9,6 +9,7 @@
 import tempfile
 
 import pyaedt
+from pyaedt.generic.constants import unit_converter
 
 # ## Define constants
 
@@ -64,7 +65,7 @@ setup.update()
 fields = m3d.ofieldsreporter
 fields.CalcStack("clear")
 fields.EnterQty("J")
-fields.EnterSurf("Coil_A2")
+# fields.EnterSurf("Coil_A2")
 fields.CalcOp("Normal")
 fields.CalcOp("Dot")
 fields.AddNamedExpression("Jn", "Fields")
@@ -74,11 +75,56 @@ fields.AddNamedExpression("Jn", "Fields")
 fields.CopyNamedExprToStack("Jn")
 fields.EnterSurf("Coil_A2")
 fields.CalcOp("Mean")
-fields.AddNamedExpression("J_avg_A2", "Fields")
+fields.AddNamedExpression("J_avg", "Fields")
 
-# Analyze setup specifying setup name.
+# ## Analyze setup specifying setup name.
 
 m3d.analyze_setup(name=setup.name)
+
+# ## Get the available report quantities
+#
+# Get available report quantities given a specific report category.
+# In this case ``Calculator Expressions`` is the category.
+
+report_types = m3d.post.available_report_types
+quantity = m3d.post.available_report_quantities(
+    report_category="Fields", quantities_category="Calculator Expressions"
+)
+
+# ## Compute the time steps of the analysis
+#
+# Create a fields report object given the nominal sweep.
+# Get the report solution data in order to compute the time steps of the transient analysis.
+
+sol = m3d.post.reports_by_category.fields(setup=m3d.nominal_sweep)
+data = sol.get_solution_data()
+time_steps = data.intrinsics["Time"]
+
+# ## Create a field plot over the coil surface and export field data
+#
+# Convert each time step into ``ms``.
+# Create a field plot on the coil surface by specifying the coil object,
+# the quantity to plot and the time step.
+# Export fields data in temporary directory as an ``.aedtplt``.
+
+for time_step in time_steps:
+    t = unit_converter(
+        time_step,
+        unit_system="Time",
+        input_units=data.units_sweeps["Time"],
+        output_units="ms",
+    )
+    m3d.post.create_fieldplot_surface(
+        assignment=m3d.modeler.objects_by_name["Coil_A2"],
+        quantity=quantity[0],
+        plot_name="J_{}_ms".format(t),
+        intrinsics={"Time": "{}ms".format(t)},
+    )
+    field_export = m3d.post.export_field_plot(
+        plot_name="J_{}_ms".format(t),
+        output_dir=temp_folder.name,
+        file_format="aedtplt",
+    )
 
 # ## Release AEDT and clean up temporary directory
 #
