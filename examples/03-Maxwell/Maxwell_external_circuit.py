6cf4fb83200b1fcb5406f931303a5f1688ca0b9f
IreneWoyna <irene.woyna@ansys.com>
2024-08-09 20:10:37 +0200

    Maxwell external circuit example

--- examples/03-Maxwell/Maxwell_external_circuit.py
+++ examples/03-Maxwell/Maxwell_external_circuit.py
@@ -5,10 +5,11 @@
 # ## Perform required imports
 
 # +
-import tempfile, os
+import tempfile
+import time
 
-from pyaedt import Maxwell3d, MaxwellCircuit
-import pyaedt
+from pyaedt import Maxwell2d
+
 
 # -
 
@@ -30,13 +31,12 @@ temp_dir = tempfile.TemporaryDirectory(suffix=".ansys")
 # +
 non_graphical = False
 
-project_name = "Transformer_zigzag"
-design_name = "1 Eddy current"
-solver = "EddyCurrent"
-desktop_version = AEDT_VERSION
+project_name = "delta_transient"
+design_name = "delta_connection"
+solver = "TransientXY"
 
-m3d = Maxwell3d(
-    version=desktop_version,
+maxwell = Maxwell2d(
+    version=AEDT_VERSION,
     new_desktop=False,
     design=design_name,
     project=project_name,
@@ -44,123 +44,108 @@ m3d = Maxwell3d(
     non_graphical=non_graphical,
 )
 
-mod = m3d.modeler
+# ## Create geometry
+#
+# Create copper coils and vacuum region, assign balloon boundary to the region edges.
 
-core_id = mod.create_udp(
-    dll="RMxprt/TransCore.dll",
-    parameters=[],
-    library="syslib",
-    name="transformer_core"
-)
-m3d.assign_material(assignment=core_id, material="steel_1008")
+coil1_id = maxwell.modeler.create_circle(orientation='Z', origin=[0, 0, 0], radius=10, name="coil1", material="copper")
+coil2_id = maxwell.modeler.create_circle(orientation='Z', origin=[25, 0, 0], radius=10, name="coil2", material="copper")
+coil3_id = maxwell.modeler.create_circle(orientation='Z', origin=[50, 0, 0], radius=10, name="coil3", material="copper")
 
-winding_size = [-5, 96]
-lv_coil_pos = [0, -21, -48]
-hv_coil_pos = [0, -31, -48]
-a_offset = [-100, 0, 0]
-c_offset = [100, 0, 0]
+region = maxwell.modeler.create_region()
+maxwell.assign_balloon(assignment=region.edges)
 
-# Create LVB and HVB windings and terminals
-lvb_terminal_id = mod.create_rectangle(orientation="YZ", origin=lv_coil_pos,
-                                       sizes=winding_size, name="LVB_terminal")
-lvb_winding_id = mod.create_rectangle(orientation="YZ", origin=lv_coil_pos, sizes=winding_size, name="LVB_winding")
-mod.sweep_around_axis(assignment=lvb_winding_id, axis="Z")
+# ## Assign excitations
+#
+# Assign external windings
 
-hvb_terminal_id = mod.create_rectangle(orientation="YZ", origin=hv_coil_pos,
-                                       sizes=winding_size, name="HVB_terminal")
-hvb_winding_id = mod.create_rectangle(orientation="YZ", origin=hv_coil_pos, sizes=winding_size, name="HVB_winding")
-mod.sweep_around_axis(assignment=hvb_winding_id, axis="Z")
+maxwell.assign_winding(assignment=["coil1"], is_solid=False, winding_type="External", name="winding1", current=0)
+maxwell.assign_winding(assignment=["coil2"], is_solid=False, winding_type="External", name="winding2", current=0)
+maxwell.assign_winding(assignment=["coil3"], is_solid=False, winding_type="External", name="winding3", current=0)
 
-lvb_winding_id.material_name = "copper"
-hvb_winding_id.material_name = "copper"
+# ## Create simulation setup
+#
+# 2 electrical periods, time step 1/20th of the el. period
 
-# duplicate = lvb_winding_id.duplicate_along_line(a_offset)
-# lva_winding_id = mod.objects[duplicate[0]]
-# lva_winding_id.name = "LVA_winding"
+setup = maxwell.create_setup()
+setup["StopTime"] = "40ms"
+setup["StepTime"] = "1ms"
 
+# ## Create external circuit
+#
+# Create circuit design with the windings
 
-# Duplicate LVB and HVB windings and terminals to create A and C
-duplications = [
-    (lvb_winding_id, a_offset, "lva_winding_id", "LVA_winding"),
-    (lvb_winding_id, c_offset, "lvc_winding_id", "LVC_winding"),
-    (lvb_terminal_id, a_offset, "lva_terminal_id", "LVA_terminal"),
-    (lvb_terminal_id, c_offset, "lvc_terminal_id", "LVC_terminal"),
-    (hvb_winding_id, a_offset, "hva_winding_id", "HVA_winding"),
-    (hvb_winding_id, c_offset, "hvc_winding_id", "HVC_winding"),
-    (hvb_terminal_id, a_offset, "hva_terminal_id", "HVA_terminal"),
-    (hvb_terminal_id, c_offset, "hvc_terminal_id", "HVC_terminal"),
-]
+# todo this only works when https://github.com/ansys/pyaedt/pull/5006 is merged
+circuit = maxwell.create_external_circuit(circuit_design="test_circuit")
 
-# Dictionary to hold the new objects
-new_objects = {}
+windings = [circuit.modeler.schematic.components[k] for k in list(circuit.modeler.schematic.components.keys())
+        if circuit.modeler.schematic.components[k].parameters["Info"] == "Winding"]
 
-for obj, offset, var_name, new_name in duplications:
-    duplicate = obj.duplicate_along_line(offset)
-    new_obj = mod.objects[duplicate[0]]
-    new_obj.name = new_name
-    new_objects[var_name] = new_obj
+# ## Finalize external circuit
+#
+# Draw rest of the components: Resistances, voltage sources, and grounds
 
-# Access the new objects using the keys
-lva_winding_id = new_objects["lva_winding_id"]
-lvc_winding_id = new_objects["lvc_winding_id"]
-lva_terminal_id = new_objects["lva_terminal_id"]
-lvc_terminal_id = new_objects["lvc_terminal_id"]
-hva_winding_id = new_objects["hva_winding_id"]
-hvc_winding_id = new_objects["hvc_winding_id"]
-hva_terminal_id = new_objects["hva_terminal_id"]
-hvc_terminal_id = new_objects["hvc_terminal_id"]
+circuit.modeler.schematic_units = "mil"
+resistance_value = 0.1
+voltage = 230
+frequency = 50
 
-lv_terminals = [lva_terminal_id, lvb_terminal_id, lvc_terminal_id]
-hv_terminals = [hva_terminal_id, hvb_terminal_id, hvc_terminal_id]
+resistors = [None]*3
+v_sources = [None]*3
+ground = [None]*3
 
-windings = [lva_winding_id, lvb_winding_id, lvc_winding_id, hva_winding_id, hvb_winding_id, hvc_winding_id]
+for i in range(3):
+    resistors[i] = circuit.modeler.schematic.create_resistor(name="R"+str(i+1), value=resistance_value,
+                                                             location=[1000, i*1000])
 
-# Create region
-mod.create_region(pad_value=50)
+    v_sources[i] = circuit.modeler.schematic.create_component(component_library="Sources", component_name="VSin",
+                                                              location=[2000, i*1000], angle=90)
+    v_sources[i].set_property(name="Va", value=voltage)
+    v_sources[i].set_property(name="VFreq", value=frequency)
+    v_sources[i].set_property(name="VPhase", value=i*120)
+    ground[i] = circuit.modeler.schematic.create_gnd([2300, i*1000], angle=90)
 
-# Assign mesh operations
-m3d.mesh.assign_length_mesh(assignment=core_id, maximum_length=50, name="core_inside")
-m3d.mesh.assign_length_mesh(assignment=windings, maximum_length=50, name="windings_inside")
+# ## Connect the components
+#
+# Draw wires
 
-# Assign coils
-lv_turns = 100
-hv_turns = 500
+windings[2].pins[1].connect_to_component(resistors[2].pins[0], use_wire=True)
+windings[1].pins[1].connect_to_component(resistors[1].pins[0], use_wire=True)
+windings[0].pins[1].connect_to_component(resistors[0].pins[0], use_wire=True)
 
-for coil in lv_terminals:
-    m3d.assign_coil(assignment=coil, conductors_number=lv_turns, name=coil.name)
+resistors[2].pins[1].connect_to_component(v_sources[2].pins[1], use_wire=True)
+resistors[1].pins[1].connect_to_component(v_sources[1].pins[1], use_wire=True)
+resistors[0].pins[1].connect_to_component(v_sources[0].pins[1], use_wire=True)
 
-for coil in hv_terminals:
-    m3d.assign_coil(assignment=coil, conductors_number=hv_turns, name=coil.name)
+circuit.modeler.schematic.create_wire(points=[resistors[2].pins[1].location, [1200, 2500], [-600, 2500],
+                                              [-600, 0], windings[0].pins[0].location])
+circuit.modeler.schematic.create_wire(points=[resistors[1].pins[1].location, [1200, 1500], [-300, 1500],
+                                              [-300, 2000], windings[2].pins[0].location])
+circuit.modeler.schematic.create_wire(points=[resistors[0].pins[1].location, [1200, 500], [-300, 500],
+                                              [-300, 1000], windings[1].pins[0].location])
 
+# ## Export and import the netlist
+#
+# Export the netlist file, and import it to Maxwell
 
-m3d.assign_winding(
-    assignment=None,
-    winding_type="External",
-    is_solid=False,
-    name="LVA",
-)
-
-m3d.add_winding_coils(assignment="LVA", coils=["LVA_terminal"])
-
-m3d.assign_winding(
-    assignment=None,
-    winding_type="External",
-    is_solid=False,
-    name="LVB",
-)
-
-solution_setup = m3d.create_setup(name="Setup1")
-
-# Create external circuit
-circuit = MaxwellCircuit(project=project_name, design="Circuit1")
-
-circuit.schematic_units = "mil"
-ind = circuit.modeler.schematic.create_inductor("Inductor1", 1.5, [-1000, 1000])
-res = circuit.modeler.schematic.create_resistor("Resistor1", 10, [1000, 1000])
-gnd = circuit.modeler.schematic.create_gnd([0.0, 0.0])
-winding1 = circuit.modeler.schematic.create_winding("Winding1")
-
-netlist_file = "C:\\export_netlist.sph"
+netlist_file = "C:\\export_netlist.sph" # todo this should be in the temp folder
 circuit.export_netlist_from_schematic(netlist_file)
 
-m3d.edit_external_circuit(netlist_file_path=netlist_file, schematic_design_name="Circuit1")
+maxwell.edit_external_circuit(netlist_file_path=netlist_file, schematic_design_name="test_circuit")
+
+# ## Analyze the setup
+#
+# Analyze the setup
+
+setup.analyze()
+
+
+# ## Release AEDT and clean up temporary directory
+#
+# Release AEDT and remove both the project and temporary directory.
+
+maxwell.release_desktop(close_projects=True, close_desktop=True)
+time.sleep(
+    3
+)  # Allow time for Electronics Desktop to close before cleaning the temporary folder.
+temp_dir.cleanup()
