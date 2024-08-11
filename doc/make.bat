4aa37ccbd78f67ea8e6ef66477cde26b30d3043a
Sébastien Morais <146729917+SMoraisAnsys@users.noreply.github.com>
2024-08-11 16:29:56 +0200

    CICD: Leverage error redirection in log (#197)

--- doc/make.bat
+++ doc/make.bat
@@ -5,7 +5,7 @@ pushd %~dp0
 REM Command file for Sphinx documentation
 
 if "%SPHINXOPTS%" == "" (
-	set SPHINXOPTS=-j auto --color -W
+	set SPHINXOPTS=-j auto --color
 )
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
