ead73081498b0ac9d021dba3bcc915dc99ece73f
Samuelopez-ansys <samuel.lopez@ansys.com>
2025-03-10 08:25:56 +0100

    Fix example style

--- examples/high_frequency/multiphysics/microwave_oven.py
+++ examples/high_frequency/multiphysics/microwave_oven.py
@@ -40,28 +40,28 @@ working_dir = tempfile.TemporaryDirectory(suffix=".ansys")
 
 
 parasolid_path = ansys.aedt.core.downloads.download_file(
-   source="oven", name="gingerbread.x_t", destination=working_dir.name
+    source="oven", name="gingerbread.x_t", destination=working_dir.name
 )
 oven_path = ansys.aedt.core.downloads.download_file(
-   source="oven", name="microwave_oven.aedt", destination=working_dir.name
+    source="oven", name="microwave_oven.aedt", destination=working_dir.name
 )
-
 
 # ### Launch HFSS
 # Open AEDT and initialize the microwave oven project.
 #
 # After the project is opened, we save it in our working directory.
 
-hfss = ansys.aedt.core.Hfss(version=AEDT_VERSION, 
-                            project=oven_path, 
-                            non_graphical=NG_MODE, 
+hfss = ansys.aedt.core.Hfss(version=AEDT_VERSION,
+                            project=oven_path,
+                            non_graphical=NG_MODE,
                             new_desktop=True)
-hfss.save_project(file_name=os.path.join(working_dir.name,'lets_cook.aedt'))
+hfss.save_project(file_name=os.path.join(working_dir.name, 'lets_cook.aedt'))
 
 # ## Model Preparation
 #
 # ### Assign material
-# This phase is fundamental because we need to assign correct material properties that are valid for both electrical and thermal analysis.
+# This phase is fundamental because we need to assign correct material properties that are valid for both electrical
+# and thermal analysis.
 #
 # PyAEDT simplifies the creation and modification of a material 
 # definitions using _getter_ and _setter_ methods. In this example we modify 5 material parameters.
@@ -74,26 +74,30 @@ ginger_material.mass_density = 1831
 ginger_material.specific_heat = 3520
 
 # ### Import the gingerbread man and assign material
-# Once the object is imported all of its properties can be edited. 
-# We are gonna move the gingerbread at the center of the plate and assign material to it.
 #
-# Finally, we are gonna change the transparency of the glass bowl.
+# Once the object is imported all of its properties can be edited. 
+# We are going to move the gingerbread at the center of the plate and assign material to it.
+#
+# Finally, we are going to change the transparency of the glass bowl.
 
 hfss.modeler.import_3d_cad(input_file=parasolid_path)
 ginger_bread = hfss.modeler["plateauPainEpices_Unnamed_5"]
-hfss.modeler.move(assignment=ginger_bread, vector=["-0.5in", "-0.2in","-38.1mm"])
-ginger_bread.material_name=ginger_material.name
+hfss.modeler.move(assignment=ginger_bread, vector=["-0.5in", "-0.2in", "-38.1mm"])
+ginger_bread.material_name = ginger_material.name
 hfss.modeler["glassBowl"].transparency = 0.75
 
 # ### Export an image
+#
 # At the end of this example we will generate a PDF report that summarizes the workflow and simulation results.
 #
 # We now save an image of the model as a PNG file to insert into the report later.
 
-hfss.post.export_model_picture(full_name=os.path.join(working_dir.name,'ginger_bread_cookie.png'))
+hfss.post.export_model_picture(full_name=os.path.join(working_dir.name, 'ginger_bread_cookie.png'))
 
 # ### Launch Icepak
-# In order to run a multiphysics analysis we need to create an Icepak project that will be retrieve the loss data from HFSS to use as a heat source.
+#
+# In order to run a multiphysics analysis we need to create an Icepak project that will be retrieve the loss data
+# from HFSS to use as a heat source.
 
 
 ipk = ansys.aedt.core.Icepak(solution_type="Transient Thermal")
@@ -110,6 +114,7 @@ exc = ipk.assign_em_losses(
 )
 
 # ### Thermal boundaries
+#
 # Main thermal boundaries will be free opening of the microwave oven. 
 #
 # In this example we set 2 different types of openings on the two faces of the oven.
@@ -121,35 +126,39 @@ ipk.assign_free_opening(assignment=ipk.modeler["ovenCavity"].top_face_y.id,
 ipk.assign_free_opening(assignment=ipk.modeler["ovenCavity"].bottom_face_y.id,
                         flow_type="Pressure")
 
-
 # #### Icepak multiple reference frame (MRF)
-# The MRF assumes mesh rotation as a solid block. In this example is useful to rotate the oven plate and cookie to reduce cooking time.
+#
+# The MRF assumes mesh rotation as a solid block.
+# In this example is useful to rotate the oven plate and cookie to reduce cooking time.
 
-rot_cyl= ipk.modeler.create_cylinder(orientation="Z",
-                                     origin=[158.75 ,228.6 ,0],
-                                     radius=110, height=150,
-                                     material="air",
-                                     name="rotating_cylinder")
-rot_cyl.display_wireframe=True
+rot_cyl = ipk.modeler.create_cylinder(orientation="Z",
+                                      origin=[158.75, 228.6, 0],
+                                      radius=110, height=150,
+                                      material="air",
+                                      name="rotating_cylinder")
+rot_cyl.display_wireframe = True
 rot_cyl.transparency = 1
-block = ipk.assign_solid_block(rot_cyl.name,0)
-block.props["Use MRF"]=True
-block.props["MRF"]="6rpm"
-block.props["Is Cylinder MRF"]=True
+block = ipk.assign_solid_block(rot_cyl.name, 0)
+block.props["Use MRF"] = True
+block.props["MRF"] = "6rpm"
+block.props["Is Cylinder MRF"] = True
 
 # ### Icepak mesh settings
+#
 # Icepak mesh settings are used to optimize the simulation time and accuracy. 
 
 ipk.mesh.global_mesh_region.manual_settings = True
 ipk.mesh.global_mesh_region.settings["MaxElementSizeX"] = "15mm"
 ipk.mesh.global_mesh_region.settings["MaxElementSizeY"] = "15mm"
 ipk.mesh.global_mesh_region.settings["MaxElementSizeZ"] = "15mm"
-ipk.mesh.global_mesh_region.settings["BufferLayers"]='2'
-ipk.mesh.global_mesh_region.settings["MaxLevels"]='2'
+ipk.mesh.global_mesh_region.settings["BufferLayers"] = '2'
+ipk.mesh.global_mesh_region.settings["MaxLevels"] = '2'
 ipk.mesh.global_mesh_region.update()
 
 # ### Icepak solution setup
-# In this example we are limiting the number of steps to a maximum of 5 steps to make the example quick to run. Ideally this number has to be increased to improve the simulation accuracy and obtain more precise results.
+#
+# In this example we are limiting the number of steps to a maximum of 5 steps to make the example quick to run.
+# Ideally this number has to be increased to improve the simulation accuracy and obtain more precise results.
 
 setup = ipk.create_setup()
 setup.props["SaveFieldsType"] = "Every N Steps"
@@ -178,8 +187,8 @@ microwave_objects = ipk.post.export_model_obj(objects, export_as_multiple_object
 
 # +
 report = AnsysReport(
-        version=ipk.aedt_version_id, design_name=ipk.design_name, project_name=ipk.project_name
-    )
+    version=ipk.aedt_version_id, design_name=ipk.design_name, project_name=ipk.project_name
+)
 report.create()
 report.add_section()
 report.add_chapter(f"Ansys GingerBread Recipe")
@@ -189,7 +198,8 @@ report.add_text("Step 1: Melt the sugar, golden syrup and butter in a saucepan, 
 report.add_text("Leave to cool for about 10 mins.")
 
 report.add_text("Step 2: Tip the flour, baking soda and spices into a large bowl.")
-report.add_text("Add the warm syrup mixture and the egg, stir everything together, then gently knead in the bowl until smooth and streak-free.")
+report.add_text(
+    "Add the warm syrup mixture and the egg, stir everything together, then gently knead in the bowl until smooth and streak-free.")
 report.add_text("The dough will firm up once cooled. Wrap in cling film and chill for at least 30 mins.")
 
 report.add_text("Step 3: Remove the dough from the fridge, leave at room temperature until softened. ")
@@ -200,7 +210,7 @@ report.add_text("An accurate Microwave Oven design requires:")
 report.add_text("1- Ansys HFSS")
 report.add_text("2- PyAEDT")
 
-report.add_image(path=os.path.join(working_dir.name,'ginger_bread_cookie.png'),
+report.add_image(path=os.path.join(working_dir.name, 'ginger_bread_cookie.png'),
                  caption="HFSS Design of Ansys Microwave Oven")
 
 report.add_page_break()
@@ -232,7 +242,9 @@ def get_average_temperature():
     df = fs.get_field_summary_data(pandas_output=True)
     return float(df["Mean"])
 
+
 # ### Method to generate streamline plot on gingerbread cookie
+#
 # This method encapsulate a set of action to plot and arrange the view of
 # the gingerbread inside the microwave oven with velocity streamline plot.
 # The view is set to front $(y-z)$.
@@ -299,6 +311,7 @@ def generate_streamline(stop):
 
 
 # ### Method to generate temperature plot on gingerbread
+#
 # This method encapsulates a set of actions to plot and arrange the view of
 # the gingerbread inside the microwave oven. The view is set to front $(y-z)$.
 
@@ -320,20 +333,25 @@ def generate_temps(stop):
     pl.camera_position = 'yz'
     pl.elevation_angle = 20
     pl.plot(export_image_path=os.path.join(working_dir.name, f'{generate_unique_name("Temperature")}.jpg'),
-               show=False)
+            show=False)
     return pl
 
 
 # ## Cook the gingerbread
+#
 # ### Loop to determine transient time
-# This is the core of our optimization process. We increase the Icepak stop time by steps of 5 seconds until the mean temperature of the gingerbread reaches the 50 degrees. We could also have used an optimizer (Optislang) or run a longer time and plot the average temperature over time.
+#
+# This is the core of our optimization process.
+# We increase the Icepak stop time by steps of 5 seconds until the mean temperature of the gingerbread reaches the
+# 50 degrees.
+# We could also have used an optimizer (Optislang) or run a longer time and plot the average temperature over time.
 
 while not solved:
     stop_time = stop_time + 5
     setup.props["Stop Time"] = f"{stop_time}s"
     setup.analyze(cores=4, tasks=4)
     mean_temperature = get_average_temperature()
-    if mean_temperature>50:
+    if mean_temperature > 50:
         solved = True
         report.add_page_break()
         report.add_sub_chapter(f"The Gingerbread is ready!!!!")
@@ -341,11 +359,13 @@ while not solved:
         plot4 = generate_temps(stop_time)
         report.add_image(plot4.image_file, f"GingerBread at the end of cooking.")
     else:
-        ipk.logger.info(f'Mean Temperature in the Gingerbread is {mean_temperature} after {stop_time}s in the microwave')
+        ipk.logger.info(
+            f'Mean Temperature in the Gingerbread is {mean_temperature} after {stop_time}s in the microwave')
         if mean_temperature > 30:
             report.add_text(f"Gingerbread is almost ready. Don't worry we will notify you when ready.")
             output_file = generate_streamline(stop_time)
-            report.add_image(os.path.join(working_dir.name, "streamlines.png"), f"GingerBread while cooking after {stop_time}s")
+            report.add_image(os.path.join(working_dir.name, "streamlines.png"),
+                             f"GingerBread while cooking after {stop_time}s")
         else:
             report.add_text(f"Take a cup of tea and relax. It will take longer.")
         ipk.save_project()
