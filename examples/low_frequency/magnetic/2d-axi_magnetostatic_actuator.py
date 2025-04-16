9d285b0f7e71202eaf009bed5347ba0ead860e82
Jonathan Velasco <jonathan.velasco@ansys.com>
2025-04-16 14:14:51 +0300

    [add][example] Correct Jupyter Notebook format

--- examples/low_frequency/magnetic/2d-axi_magnetostatic_actuator.py
+++ examples/low_frequency/magnetic/2d-axi_magnetostatic_actuator.py
@@ -1,16 +1,11 @@
-# # 2D Axi-symmetric Actuator - Force Calculation
+# # 2D Axi-symmetric Actuator
 #
 # This example demonstrates how to leverage both: axi-symmetry and the magnetostatic solver
 # to calculate the forces experienced by the anchor of an actuator due to change in
 # current and anchor location
 #
-# Most examples can be described as a series of steps that comprise a workflow.
-# 1. Import packages and instantiate the application.
-# 2. Do something useful and interesting like creating the model, assign materials and boundary conditions, etc.
-# 3. Run one or more analyses.
-# 4. View the results.
-#
-# Keywords: **Maxwell2D**, **axi-symmetry** **magnetostatic**, **translational motion**, **installation example**
+# Keywords: **Maxwell2D**, **axi-symmetry** **magnetostatic**, **translational motion**, **parametric sweep**,
+# **installation example**
 
 # ## Prerequisites
 #
@@ -89,8 +84,8 @@ m2d[r"Amp_1"] = "1000A"  # Net current applied to coil
 m2d[r"move"] = "0mm"  # Displacement applied to anchor
 
 # ### Create 2D model
-#
 # Build coil, anchor and housing geometries
+
 coil_m = m2d.modeler.create_rectangle(
     origin=[r"3mm", r"0mm", r"7mm"], sizes=[-14, 6], name=r"Coil", material=r"Copper"
 )
@@ -106,10 +101,12 @@ housing_m = m2d.modeler.create_polyline(
 m2d.modeler.cover_lines(housing_m)
 
 # Create surrounding vacuum domain
+
 region_m = m2d.modeler.create_region(pad_percent=100)
 region_m.material_name = r"vacuum"
 
 # Fit all geometrical entities into the modeler's window
+
 m2d.modeler.fit_all()
 
 # ### Assign boundary conditions
@@ -117,21 +114,24 @@ m2d.modeler.fit_all()
 # Apply zero magnetic vector potential on the region edges
 # that the field only has a tangential component at the edge of the
 # surrounding domain/region
+
 m2d.assign_vector_potential(assignment=region_m.edges, boundary=r"VectorPotential1")
 
 # ### Assign Excitation
 #
 # Create a current driven coil by applying a current excitation in the coil domain
+
 m2d.assign_current(assignment=coil_m.name, amplitude=r"Amp_1", name="Current1")
 
 # ### Enable force calculation on the anchor
+
 m2d.assign_force(
     assignment=anchor_m,
     force_name="Force",
 )
 
 # ### Define solution setup
-#
+
 setup = m2d.create_setup(r"MySetup")
 print(setup.props)
 setup.props[r"MaximumPasses"] = 15
@@ -143,6 +143,7 @@ setup.props[r"RelativeResidual"] = 1e-6
 # ### Create variable/parameter sweeps
 #
 # Enable sweeps over coil net current and anchor displacement
+
 value_sweep = m2d.parametrics.add(
     r"Amp_1", 500, 2000, 500, name="ParametricSetup1", variation_type="LinearStep"
 )
@@ -150,15 +151,13 @@ value_sweep.add_variation(r"move", 0, 4, 1, variation_type="LinearStep")
 #
 
 # ### Run analysis
-#
-# > Run the simulation.
 
 value_sweep.analyze(cores=NUM_CORES)
 
 # ## Postprocess
 #
 # Create a Force vs. move (displacement) Report/Plot for every current value
-#
+
 m2d.post.create_report(
     expressions=[r"Force.Force_z"],
     variations={r"Amp_1": r"All", r"move": r"All"},
