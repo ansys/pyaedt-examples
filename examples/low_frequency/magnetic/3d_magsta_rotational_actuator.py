bbfad3fdba86cf275e43791c0b6f0033cfa50f90
Jonathan Velasco <jonathan.velasco@ansys.com>
2025-05-02 17:05:08 +0300

    [example][magnetic] 3D Rotational Actuator example Initial entry

---
+++ examples/low_frequency/magnetic/3d_magsta_rotational_actuator.py
@@ -0,0 +1,243 @@
+# # 3D Rotational Actuator (Magnetostatic)
+#
+# (do not specify the solver type in the title, i.e.: Maxwell2D, Maxwell3D etc)
+#
+# Most examples can be described as a series of steps that comprise a workflow.
+# 1. Import packages and instantiate the application.
+# 2. Do something useful and interesting like creating the model, assing materials and boundary conditions, etc.
+# 3. Run one or more analyses.
+# 4. View the results.
+#
+# Keywords: **Template**, **Jupyter**
+
+# ## Prerequisites
+#
+# ### Perform imports
+
+# +
+import os
+import tempfile
+import time
+import csv
+
+import ansys.aedt.core  # Interface to Ansys Electronics Desktop
+# -
+
+# def uncover_faces(object):
+#     m3d.oeditor.UncoverFaces(
+#         [
+#             "NAME:Selections",
+#             "Selections:="	, object.name,
+#             "NewPartsModelFlag:="	, "Model"
+#         ],
+#         [
+#             "NAME:Parameters",
+#             [
+#                 "NAME:UncoverFacesParameters",
+#                 "FacesToUncover:="	, [int(str(object.faces[0]))]
+#             ]
+#         ])
+
+
+
+# ### Define constants
+# Constants help ensure consistency and avoid repetition throughout the example.
+
+AEDT_VERSION = "2025.1"
+NUM_CORES = 4
+NG_MODE = False  # Open AEDT UI when it is launched.
+
+
+# ### Create temporary directory
+#
+# Create a temporary working directory.
+# The name of the working folder is stored in ``temp_folder.name``.
+#
+# > **Note:** The final cell in the notebook cleans up the temporary folder. If you want to
+# > retrieve the AEDT project and data, do so before executing the final cell in the notebook.
+
+temp_folder = tempfile.TemporaryDirectory(suffix=".ansys")
+
+# ### Launch Maxwell 3d
+# Create an instance of the ``Maxwell3d`` class. The Ansys Electronics Desktop will be launched
+# with an active Maxwell2D design. The ``m3d`` object is subsequently used to create and simulate the actuator model.
+
+project_name = os.path.join(temp_folder.name, "rotational_actuator.aedt")
+m3d = ansys.aedt.core.Maxwell3d(
+    project=project_name,
+    design="3d_magsta_actuator",
+    solution_type="Magnetostatic",
+    version=AEDT_VERSION,
+    non_graphical=NG_MODE,
+    new_desktop=True,
+)
+
+# ## Model Preparation
+#
+# ### Declare and initialize design parameters
+
+m3d["angle"] = "29deg"
+
+# ### Create 3D model
+#
+# #### Set model units
+
+m3d.modeler.model_units = "mm"
+
+# #### Create custom material
+bh_curve = []
+with open("bh_file.tab") as f:
+    reader = csv.reader(f, delimiter="\t")
+    next(reader)
+    for row in reader:
+        bh_curve.append([float(row[0]), float(row[1])])
+
+arm_steel = m3d.materials.add_material(name="arm_steel")
+arm_steel.conductivity = 2000000
+arm_steel.permeability.value = bh_curve
+
+# #### Create outer arm
+
+outer_arm = m3d.modeler.create_cylinder(orientation=ansys.aedt.core.constants.AXIS.Z ,origin=[0,0,0],radius=104.5,height=25.4,num_sides=0, name="Outer_arm", material=arm_steel.name)
+cylinder_tool = m3d.modeler.create_cylinder(orientation=ansys.aedt.core.constants.AXIS.Z ,origin=[0,0,0],radius=83.1,height=25.4,num_sides=0, name="Cylinder_tool")
+m3d.modeler.subtract([outer_arm],[cylinder_tool], keep_originals=False)
+box_1 = m3d.modeler.create_box(origin=[-13.9 ,0 ,0],sizes=[27.8,-40,25.4], name="Box1")
+m3d.modeler.move(box_1,vector=[0,-45,0])
+m3d.modeler.duplicate_and_mirror(assignment=box_1,origin=[0,0,0],vector=[0,1,0])
+m3d.modeler.unite([outer_arm, box_1.name, box_1.name+"_1"])
+cylinder_1 = m3d.modeler.create_cylinder(orientation=ansys.aedt.core.constants.AXIS.Z ,origin=[0,0,0],radius=53.75,height=25.4,num_sides=0, name="Cylinder1")
+m3d.modeler.subtract([outer_arm],[cylinder_1], keep_originals=False)
+outer_arm.color="(192 192 192)"
+
+# #### Create inner arm
+
+inner_arm = m3d.modeler.create_cylinder(orientation=ansys.aedt.core.constants.AXIS.Z ,origin=[0,0,0],radius=38.1,height=25.4,num_sides=0, name="Inner_arm", material=arm_steel.name)
+shaft = m3d.modeler.create_cylinder(orientation=ansys.aedt.core.constants.AXIS.Z ,origin=[0,0,0],radius=25.4,height=25.4,num_sides=0, name="shaft")
+m3d.modeler.subtract([inner_arm],[shaft], keep_originals=False)
+
+box_2 = m3d.modeler.create_box(origin=[-12.7 ,0 ,0],sizes=[25.4,-20,25.4], name="Box2")
+m3d.modeler.move(box_2,vector=[0,-35,0])
+m3d.modeler.duplicate_and_mirror(assignment=box_2,origin=[0,0,0],vector=[0,1,0])
+m3d.modeler.unite([inner_arm, box_2.name, box_2.name+"_1"])
+
+finalpole2 = m3d.modeler.create_cylinder(orientation=ansys.aedt.core.constants.AXIS.Z ,origin=[0,0,0],radius=51.05,height=25.4,num_sides=0, name="finalpole2")
+m3d.modeler.intersect(assignment=[inner_arm, finalpole2])
+
+inner_arm.color="(192 192 192)"
+
+
+# #### Create a local/relative coordinate system
+
+m3d.modeler.create_coordinate_system(origin=[0 ,0 ,12.7],reference_cs="Global",name="RelativeCS1",mode="axis",x_pointing=[1 ,0 ,0],y_pointing=[0 ,1 ,0])
+
+# #### Apply rotation on relative coordinate system
+
+m3d.modeler.rotate(assignment=[inner_arm], axis="RelativeCS1" , angle="angle")
+
+
+# #### Create coils
+coil1 = m3d.modeler.create_rectangle(orientation=ansys.aedt.core.constants.AXIS.X,origin=[0,0,15.5],sizes=[17,24], name="coil1", material="copper")
+coil1.color="(249 186 70)"
+path_rectangle = m3d.modeler.create_rectangle(orientation=ansys.aedt.core.constants.AXIS.Y,origin=[-17,0,-15.5],sizes=[31,34], name="path")
+m3d.modeler.uncover_faces(path_rectangle) # TODO: Implement function in pyaedt
+m3d.modeler.sweep_along_path(assignment=coil1, sweep_object=path_rectangle)
+round = m3d.modeler.create_cylinder(orientation=ansys.aedt.core.constants.AXIS.Y ,origin=[0,0,0],radius=46.238512086788,height=17,num_sides=0, name="Round")
+m3d.modeler.intersect(assignment=[coil1, round])
+m3d.modeler.move(assignment=coil1,vector=[0,54.5,0])
+m3d.modeler.duplicate_and_mirror(assignment=coil1,origin=[0,0,0],vector=[0,1,0])
+m3d.modeler.section(assignment=coil1,plane='XY')
+m3d.modeler.section(assignment=coil1.name + "_1",plane='XY')
+
+# #### Create air region
+bgnd = m3d.modeler.create_box(origin=[-250 ,-250 ,-250],sizes=[500,500,500], name="bgnd")
+bgnd.transparency = 1
+
+# #### Create Coil Terminals by Separating Sheet bodies
+coil_terminal1 = coil1.name + "_1_Section1_Separate1"
+coil_terminal2 = coil1.name + "_Section1"
+m3d.modeler.separate_bodies(assignment=coil_terminal2)
+m3d.modeler.delete(assignment=coil1.name + "_Section1_Separate1")
+m3d.modeler.separate_bodies(assignment=coil1.name + "_1_Section1")
+m3d.modeler.delete(assignment=coil1.name + "_1_Section1")
+
+m3d.modeler.fit_all()
+
+# ### Assign boundary conditions
+#
+m3d.assign_current(assignment=coil_terminal1, amplitude=675.5, solid=False, name="Current_1")
+m3d.assign_current(assignment=coil_terminal2, amplitude=675.5, solid=False, name="Current_2")
+# ### Define solution setup
+#
+m3d.assign_matrix(assignment=["Current_1", "Current_2"],matrix_name="Matrix1")
+m3d.assign_torque(assignment=inner_arm.name, is_virtual=True, coordinate_system="Global", axis="Z", torque_name="Virtual_Torque")
+
+setup = m3d.create_setup("MySetup")
+print(setup.props)
+setup.props["MaximumPasses"] = 10
+setup.props["PercentRefinement"] = 30
+setup.props["PercentError"] = 1
+setup.props["MinimumPasses"] = 2
+setup.props["RelativeResidual"] = 1e-3
+
+parametric_sweep = m3d.parametrics.add(variable="angle",start_point="0", end_point="30", step="5",
+                                       variation_type="LinearStep",name="ParametricSetup1")
+parametric_sweep.add_calculation(calculation="Virtual_Torque.Torque")
+parametric_sweep.add_calculation(calculation="Matrix1.L(Current_1, Current_1)")
+parametric_sweep.add_calculation(calculation="Matrix1.L(Current_1, Current_2)")
+parametric_sweep.add_calculation(calculation="Matrix1.L(Current_2, Current_1)")
+parametric_sweep.add_calculation(calculation="Matrix1.L(Current_2, Current_2)")
+parametric_sweep.props["ProdOptiSetupDataV2"]["SaveFields"] = True
+
+
+
+m3d.post.create_report(
+    expressions=["Matrix1.L(Current_1, Current_1)",
+                 "Matrix1.L(Current_1, Current_2)",
+                 "Matrix1.L(Current_2, Current_1)",
+                 "Matrix1.L(Current_2, Current_2)"],
+    variations={"angle": "All"},
+    plot_name="XY Plot 1",
+    primary_sweep_variable="angle",
+    plot_type="Rectangular Plot",
+)
+#
+# ### Run analysis
+#
+parametric_sweep.analyze(cores=NUM_CORES)
+
+# m3d.analyze_setup("Setup1")
+
+# ## Postprocess
+#
+# After generating results demonstrate how to visualize and evaluate results
+# in this section.
+# Level 3 headers can be used to identify various post-processing
+# steps.
+#
+# ### Evaluate loss
+m3d.post.create_fieldplot_surface(
+    assignment=[inner_arm, outer_arm], quantity="Mag_B", plot_name="Mag_B1", field_type="Fields")
+
+m3d.post.create_fieldplot_surface(
+    assignment=[inner_arm, outer_arm], quantity="B_Vector", plot_name="B_Vector1", field_type="Fields")#
+# ### Visualize fields
+# > PyAEDT provides access to field solution data via the
+
+
+# ## Finish
+#
+# ### Save the project
+
+m3d.save_project()
+m3d.release_desktop(False, False)
+# Wait 3 seconds to allow AEDT to shut down before cleaning the temporary directory.
+time.sleep(3)
+
+# ### Clean up
+#
+# All project files are saved in the folder ``temp_folder.name``.
+# If you've run this example as a Jupyter notebook, you
+# can retrieve those project files. The following cell
+# removes all temporary files, including the project folder.
+
+temp_folder.cleanup()
