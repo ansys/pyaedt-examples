aa53074aea04d95f3a50e4416821bdb04f1873ad
Sébastien Morais <sebastien.morais@ansys.com>
2025-09-01 16:56:37 +0200

    REFACTOR: Clean GDS workflow recent changes

--- examples/edb/legacy_standalone/gds_workflow.py
+++ examples/edb/legacy_standalone/gds_workflow.py
@@ -19,12 +19,11 @@ import tempfile
 from pyedb.dotnet.edb import Edb
 from pyedb.misc.downloads import download_file
 from ansys.aedt.core.hfss3dlayout import  Hfss3dLayout
-from pyedb.generic.settings import Settings
 
 # ### Define constants
 # Constants help ensure consistency and avoid repetition throughout the example.
 
-Settings.specified_version = "2025.2"
+AEDT_VERSION = "2025.2"
 NG_MODE = False  # Open AEDT UI when it is launched.
 
 # ### Create temporary directory
@@ -94,7 +93,7 @@ edb.close()
 #
 # <img src="_static/layout.png" width="800">
 
-h3d = Hfss3dLayout(project=edb_path, version=Settings.specified_version, new_desktop=NG_MODE)
+h3d = Hfss3dLayout(project=edb_path, version=AEDT_VERSION, new_desktop=NG_MODE)
 
 # ### Close the HFSS 3D Layout 
 # The following command releases Ansys Electronics Desktop and closes the project.
