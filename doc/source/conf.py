ca1ae59cb0b13bffabc1b2f4a579e1b108af8216
raghajaf <ramin.aghajafari@ansys.com>
2025-11-07 14:40:35 -0500

    Added filter examples to CI pipeline for testing purpose.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -284,10 +284,10 @@ def convert_examples_into_notebooks(app):
         "template.py",
         "gui_manipulation.py",
         "electrothermal.py",
-        # GitHub CI/CD Pipeline fails to run lumped_element.py but it runs locally. Will be included for 26R1 relase
-        "lumped_element.py",
-        # GitHub CI/CD Pipeline fails to run distributed_filter.py but it runs locally. Will be included for 26R1 relase 
-        "distributed_filter.py",
+        # GitHub CI/CD Pipeline fails to run lumped_element.py but it runs locally.
+        # "lumped_element.py",
+        # GitHub CI/CD Pipeline fails to run distributed_filter.py but it runs locally.
+        # "distributed_filter.py",
         # TODO: Remove once EMIT examples are moved into extensions.
         "interference_type.py",
         "interference.py",
