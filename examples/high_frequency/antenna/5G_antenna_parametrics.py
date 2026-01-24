5eaa94f766e2d4f98943c9f3e5489d0e620ded13
Devin <38879940+Devin-Crawford@users.noreply.github.com>
2026-01-24 11:08:53 +0100

    Update examples/high_frequency/antenna/5G_antenna_parametrics.py
    Co-authored-by: Sébastien Morais <146729917+SMoraisAnsys@users.noreply.github.com>

--- examples/high_frequency/antenna/5G_antenna_parametrics.py
+++ examples/high_frequency/antenna/5G_antenna_parametrics.py
@@ -20,7 +20,6 @@ import pyedb
 # Constants help ensure consistency and avoid repetition throughout the example.
 
 AEDT_VERSION = "2025.2"
-NUM_CORES = 4
 NG_MODE = False  # Open AEDT UI when it is launched.
 
 # ### Create temporary directory
