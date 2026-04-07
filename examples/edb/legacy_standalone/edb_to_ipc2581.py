05b3661d4ed01fcbb634fb04e4ce4e98b7e0794d
Samuelopez-ansys <samuel.lopez@ansys.com>
2026-04-07 14:47:56 +0200

    Move to 2026R1

--- examples/edb/legacy_standalone/edb_to_ipc2581.py
+++ examples/edb/legacy_standalone/edb_to_ipc2581.py
@@ -31,7 +31,7 @@ print(targetfile)
 # Select EDB version (change it manually if needed, e.g. "2025.1")
 AEDT_VERSION = "2026.1"
 
-edb = pyedb.Edb(edbpath=targetfile, version=edb_version)
+edb = pyedb.Edb(edbpath=targetfile, version=AEDT_VERSION)
 # -
 
 # ## Parametrize the width of a trace.
