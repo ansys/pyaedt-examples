66ea4f8ffd92f185f9908b410bb219a8cb81259f
Eduardo Blanco <eduardo.blancofernandez@ansys.com>
2026-05-28 10:01:48 +0200

    Reverted

--- examples/high_frequency/antenna/array.py
+++ examples/high_frequency/antenna/array.py
@@ -231,6 +231,8 @@ ffdata.plot_cut(
 
 array_farfield_data.plot_3d(
     quantity="RealizedGain",
+    output_file=os.path.join(working_directory, "Image.jpg"),
+    show=False,
 )
 
 # ### Clean up
