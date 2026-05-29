3db7e2a81187960b2d17656e8d11b993b4d77b55
slopez <samuel.lopez@synopsys.com>
2026-05-29 11:22:48 +0200

    Deprecated argument

--- examples/high_frequency/multiphysics/microwave_oven.py
+++ examples/high_frequency/multiphysics/microwave_oven.py
@@ -222,7 +222,7 @@ def get_average_temperature():
     fs = ipk.post.create_field_summary()
     fs.add_calculation(entity="Object", geometry="Surface", geometry_name=ginger_bread_thermal.name, quantity="Temperature", time=f"{stop_time}s")
     df = fs.get_field_summary_data(pandas_output=True)
-    return float(df["Mean"])
+    return float(df["Mean"].iloc[0])
 
 
 # ### Method to generate streamline plot on gingerbread cookie
