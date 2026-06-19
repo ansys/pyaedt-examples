cbefcc7835e82a8bbae10943169874dd38b3f42d
Giulia Malinverno <103059376+gmalinve@users.noreply.github.com>
2026-06-19 09:32:00 +0200

    Apply suggestion from @SMoraisAnsys
    Co-authored-by: Sébastien Morais <146729917+SMoraisAnsys@users.noreply.github.com>

--- examples/low_frequency/general/resistance.py
+++ examples/low_frequency/general/resistance.py
@@ -10,6 +10,11 @@
 # This example uses PyAEDT to set up a resistance calculation
 # and solve it using the Maxwell 2D DCConduction solver.
 #
+# # Resistance calculation
+#
+# This example uses PyAEDT to set up a resistance calculation
+# and solve it using the Maxwell 2D DCConduction solver.
+#
 # Keywords: **Maxwell 2D**, **DXF import**, **material sweep**, **expression cache**.
 
 # ## Perform imports and define constants
