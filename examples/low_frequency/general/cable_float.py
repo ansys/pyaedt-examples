1401fc44eaaaaa9533c17a01ec3273486d5c3da0
Sébastien Morais <sebastien.morais@synopsys.com>
2026-06-24 10:22:09 +0200

    docs: update cable floating content

--- examples/low_frequency/general/cable_float.py
+++ examples/low_frequency/general/cable_float.py
@@ -18,12 +18,13 @@
 
 # +
 
+from dataclasses import dataclass
 import os
 import tempfile
 import time
+from pathlib import Path
 
 import ansys.aedt.core
-from pathlib import Path
 from ansys.aedt.core.examples.downloads import download_file
 
 # -
@@ -46,7 +47,7 @@ data_folder = Path(download_file(r"pyaedt/maxwell_power_cable", local_path=temp_
 json_path = data_folder / JSON_FILENAME
 
 # ## Set up for model creation
-
+#
 # Start an instance of Maxwell 2d, providing the version, project name, design
 # name, and type.
 
@@ -78,33 +79,36 @@ m2d.modeler.create_coordinate_system(origin=["x0_cond", "y0_cond", "0mm"], name=
 m2d.modeler.set_working_coordinate_system("CS_cond_1")
 
 # Create cable layer object
-class Cable_Layer:
-    def __init__(self, radius, name, material, color):
-        self.radius = radius
-        self.name = name
-        self.material = material
-        self.color = color
+@dataclass
+class CableLayer:
+    radius: str
+    name: str
+    material: str
+    color: tuple
 
 # Create layer instances
-obj_cond_1= Cable_Layer("dia_conductor/2","cond_1","copper",(255,0,0))
-obj_cond_screen_1 = Cable_Layer("dia_conductor_screen/2","cond_screen_1","copper",(255,0,0))
-obj_cond_ins_1 = Cable_Layer("dia_conductor_insulation/2","cond_insulation_1","XLPE",(255,255,0))
-obj_cond_ins_screen_1 = Cable_Layer("dia_conductor_insulation_screen/2","cond_insulation_screen_1","copper",(0, 128, 128))
-obj_cond_sheat_1 = Cable_Layer("dia_conductor_sheat/2","cond_dia_conductor_sheat_1","MDPE",(143,159,165))
-cond_layers = [obj_cond_1,
-               obj_cond_screen_1,
-               obj_cond_ins_1,
-               obj_cond_ins_screen_1,
-               obj_cond_sheat_1,
-               ]
+obj_cond_1 = CableLayer("dia_conductor/2", "cond_1", "copper", (255,0,0))
+obj_cond_screen_1 = CableLayer("dia_conductor_screen/2", "cond_screen_1", "copper", (255,0,0))
+obj_cond_ins_1 = CableLayer("dia_conductor_insulation/2", "cond_insulation_1", "XLPE", (255,255,0))
+obj_cond_ins_screen_1 = CableLayer("dia_conductor_insulation_screen/2", "cond_insulation_screen_1", "copper", (0, 128, 128))
+obj_cond_sheat_1 = CableLayer("dia_conductor_sheat/2", "cond_dia_conductor_sheat_1", "MDPE", (143,159,165))
+cond_layers = [
+    obj_cond_1,
+    obj_cond_screen_1,
+    obj_cond_ins_1,
+    obj_cond_ins_screen_1,
+    obj_cond_sheat_1,
+]
 
 # Create conductor assembly layer by layer
-cond_ids = [m2d.modeler.create_circle(
-    origin=["0mm", "0mm", "0mm"],
-    radius = layer.radius,
-    name = layer.name,
-    material = layer.material,
-    color = layer.color)
+cond_ids = [
+    m2d.modeler.create_circle(
+        origin = ["0mm", "0mm", "0mm"],
+        radius = layer.radius,
+        name = layer.name,
+        material = layer.material,
+        color = layer.color
+    )
     for layer in cond_layers
 ]
 
@@ -116,31 +120,33 @@ m2d.modeler.duplicate_around_axis(all_obj_names, axis="Z", angle=120, clones=3)
 # Create the geometry for the outer structures.
 
 # Create layer instances
-obj_filler= Cable_Layer ("dia_filler/2","filler_background","MDPE", (175,143,175))
-obj_bedding = Cable_Layer("dia_bedding/2","bedding","PPY",(128,0,255))
-obj_inner_sheat = Cable_Layer("dia_inner_sheath/2","inner_sheath","HDPE",(128,0,255))
-obj_inner_armor = Cable_Layer("dia_armor_inner/2","inner_armor","stainless_steel",(128,0,255))
-obj_armor_bedding = Cable_Layer("dia_bedding_armor/2","bedding_armor","PPY",(128,0,255))
-obj_outer_armor = Cable_Layer("dia_armor_outer/2","outer_armour","stainless_steel",(128,0,255))
-obj_outer_sheat = Cable_Layer("dia_outer_sheat/2","outer_sheat","HDPE",(128,0,255))
-outer_layers = [obj_filler,
-                obj_bedding,
-                obj_inner_sheat,
-                obj_inner_armor,
-                obj_armor_bedding,
-                obj_outer_armor,
-                obj_outer_sheat,
-                ]
+obj_filler= CableLayer ("dia_filler/2","filler_background","MDPE", (175,143,175))
+obj_bedding = CableLayer("dia_bedding/2","bedding","PPY",(128,0,255))
+obj_inner_sheat = CableLayer("dia_inner_sheath/2","inner_sheath","HDPE",(128,0,255))
+obj_inner_armor = CableLayer("dia_armor_inner/2","inner_armor","stainless_steel",(128,0,255))
+obj_armor_bedding = CableLayer("dia_bedding_armor/2","bedding_armor","PPY",(128,0,255))
+obj_outer_armor = CableLayer("dia_armor_outer/2","outer_armour","stainless_steel",(128,0,255))
+obj_outer_sheat = CableLayer("dia_outer_sheat/2","outer_sheat","HDPE",(128,0,255))
+outer_layers = [
+    obj_filler,
+    obj_bedding,
+    obj_inner_sheat,
+    obj_inner_armor,
+    obj_armor_bedding,
+    obj_outer_armor,
+    obj_outer_sheat,
+]
 
 # Create outer assembly layer by layer
 
-outer_ids = [m2d.modeler.create_circle(
-    origin=["0mm", "0mm", "0mm"],
-    radius = layer.radius,
-    name = layer.name,
-    material = layer.material,
-    color = layer.color)
-    for layer in outer_layers
+outer_ids = [
+    m2d.modeler.create_circle(
+        origin = ["0mm", "0mm", "0mm"],
+        radius = layer.radius,
+        name = layer.name,
+        material = layer.material,
+        color = layer.color
+    ) for layer in outer_layers
 ]
 
 
@@ -150,23 +156,34 @@ outer_ids = [m2d.modeler.create_circle(
 m2d.modeler.copy(outer_ids[0])
 filler_background_water = m2d.modeler.paste()
 m2d.assign_material(filler_background_water,"water_sea")
-m2d.modeler.create_coordinate_system(origin=["0mm", "0mm", "0mm"],
-                                     mode= "axis",
-                                     x_pointing = ["cos(30deg)", "-sin(30deg)", "0mm"],
-                                     y_pointing = ["sin(30deg)", "cos(30deg)", "0mm"],
-                                     name="CS_rot_1")
+m2d.modeler.create_coordinate_system(
+    origin = ["0mm", "0mm", "0mm"],
+    mode = "axis",
+    x_pointing = ["cos(30deg)", "-sin(30deg)", "0mm"],
+    y_pointing = ["sin(30deg)", "cos(30deg)", "0mm"],
+    name = "CS_rot_1"
+)
 m2d.modeler.split(outer_ids[0],"ZX")
-m2d.modeler.create_coordinate_system(origin=["0mm", "0mm", "0mm"],
-                                     mode= "axis",
-                                     x_pointing = ["-cos(30deg)", "-sin(30deg)", "0mm"],
-                                     y_pointing = ["-sin(30deg)", "cos(30deg)", "0mm"],
-                                     name="CS_rot_2")
+m2d.modeler.create_coordinate_system(
+    origin = ["0mm", "0mm", "0mm"],
+    mode = "axis",
+    x_pointing = ["-cos(30deg)", "-sin(30deg)", "0mm"],
+    y_pointing = ["-sin(30deg)", "cos(30deg)", "0mm"],
+    name = "CS_rot_2"
+)
 m2d.modeler.split(outer_ids[0],"ZX")
 m2d.modeler.delete_objects_containing("Split")
 m2d.modeler.set_working_coordinate_system("Global")
-tool_ellipse = m2d.modeler.create_ellipse(origin = ["0mm","-65mm","0mm"],major_radius=60, ratio =0.55)
+tool_ellipse = m2d.modeler.create_ellipse(
+    origin = ["0mm","-65mm","0mm"],
+    major_radius = 60,
+    ratio = 0.55
+)
 m2d.modeler.intersect([outer_ids[0],tool_ellipse])
-m2d.modeler.subtract([outer_ids[0]],["cond_dia_conductor_sheat_1","cond_dia_conductor_sheat_1_2"])
+m2d.modeler.subtract(
+    [outer_ids[0]],
+    ["cond_dia_conductor_sheat_1","cond_dia_conductor_sheat_1_2"]
+)
 m2d.modeler.duplicate_around_axis(outer_ids[0], axis="Z", angle=120, clones=3)
 
 # ## Release AEDT
