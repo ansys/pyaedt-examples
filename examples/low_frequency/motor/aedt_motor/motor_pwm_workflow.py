c2bc22fdb493194710d5130806cbaf3a8f71c9e7
MCARBONI1992 <mcarboni@synopsys.com>
2026-09-18 16:45:39 +0200

    pwm example

--- examples/low_frequency/motor/aedt_motor/motor_pwm_workflow.py
+++ examples/low_frequency/motor/aedt_motor/motor_pwm_workflow.py
@@ -1,3 +1,25 @@
+# Copyright (C) 2024 - 2026 Synopsys, Inc. and ANSYS, Inc. All rights reserved.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # # PWM Losses Calculation in a Permanent Magnet Machine
 #
 # This example covers the workflow to compute the PWM losses in an IPM motor, using the Maxwell 2D model and the SVPWM
@@ -38,6 +60,7 @@ from ansys.aedt.core.generic.numbers_utils import Quantity
 #
 # The following function is used to translate a python dictionary into a .csv file. It is used for the optimetrics
 # definitions
+
 
 def write_opt_csv(var_dict, filename):
     headers = list(var_dict.keys())
